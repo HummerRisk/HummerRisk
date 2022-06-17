6c55087e5be2514b18118f86709c2a0e6dbcdebe
harris <ma402847591@gmail.com>
2022-06-17 16:12:07 +0800

    fix(vue): 解决前端打包warning报错问题

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
-const path = require('path')
+const path = require('path');
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
 
 module.exports = {
@@ -13,6 +13,21 @@ module.exports = {
         '@': resolve('src')
       }
     },
+    //该选项可以控制 webpack 如何通知「资源(asset)和入口起点超过指定文件限制」
+    performance: {
+      hints: "warning", // "warning"枚举，"error"性能提示中抛出错误，false 关闭性能提示
+      maxAssetSize: 20000000, // 整数类型（以字节为单位）入口起点的最大体积
+      maxEntrypointSize: 40000000, // 整数类型（以字节为单位）生成文件的最大体积
+      assetFilter: function(assetFilename) { // 只给出 js 文件的性能提示
+        // 提供资源文件名的断言函数
+        return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+      }
+    },
+  },
+  css: {
+    extract: {
+      ignoreOrder: true
+    }
   },
   devServer: {
     port: 8080,
