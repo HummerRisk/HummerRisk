4a30c39a46485623757f1d9a18334cfbdf14c59d
harris <ma402847591@gmail.com>
2022-08-24 21:18:44 +0800

    fix(event): 解决报错，引用冲突

--- backend/src/main/java/com/hummerrisk/service/CloudEventService.java
+++ backend/src/main/java/com/hummerrisk/service/CloudEventService.java
@@ -45,133 +45,133 @@ public class CloudEventService {
     @Resource
     private PlatformTransactionManager transactionManager;
 
-//    public List<CloudEvent> getCloudEvents(String accountId,String region,String startTime,String endTime){
-//        CloudEventExample cloudEventExample = new CloudEventExample();
-//        cloudEventExample.createCriteria().andCloudAccountIdEqualTo(accountId).andSyncRegionEqualTo(region)
-//                .andEventTimeBetween(DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",startTime),DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",endTime));
-//        return cloudEventMapper.selectByExample(cloudEventExample);
-//    }
-//    public void syncCloudEvents(String accountId,String region,String startTime,String endTime){
-//        CloudEventSyncLog cloudEventSyncLog = new CloudEventSyncLog();
-//        cloudEventSyncLog.setAccountId(accountId);
-//        cloudEventSyncLog.setRegion(region);
-//        cloudEventSyncLog.setCreateTime(DateUtils.getNowDate());
-//        cloudEventSyncLog.setRequestStartTime(DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",startTime));
-//        cloudEventSyncLog.setRequestEndTime(DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",endTime));
-//        cloudEventSyncLog.setStatus(0);
-//        CloudEventSyncLogExample cloudEventSyncLogExample = new CloudEventSyncLogExample();
-//        cloudEventSyncLogExample.createCriteria().andStatusEqualTo(0).andAccountIdEqualTo(accountId)
-//                .andRegionEqualTo(region);
-//        List<CloudEventSyncLog> cloudEventSyncLogs = cloudEventSyncLogMapper.selectByExample(cloudEventSyncLogExample);
-//        if(cloudEventSyncLogs.size()>0){
-//            throw new RuntimeException("task running!");
-//        }
-//        cloudEventSyncLogMapper.insertSelective(cloudEventSyncLog);
-//        commonThreadPool.addTask(() -> {
-//            saveCloudEvent(cloudEventSyncLog.getId(),accountId,region,startTime,endTime);
-//        });
-//    }
-//
-//    public void saveCloudEvent(Integer logId,String accountId, String region, String startTime, String endTime){
-//        try{
-//            AccountWithBLOBs account = accountService.getAccount(accountId);
-//            Map<String, String> accountMap = PlatformUtils.getAccount(account, region, proxyMapper.selectByPrimaryKey(account.getProxyId()));
-//            List<CloudEvent> result = new ArrayList<>();
-//            int pageNum = 1;
-//            int maxNum = 50;
-//            boolean haveNextPage = true;
-//            while (haveNextPage){
-//                List<CloudEvent> pageResult = getCloudEvents(account,accountMap,startTime,endTime,pageNum,maxNum);
-//                result.addAll(pageResult);
-//                if(pageResult.size() < maxNum || pageNum > MAX_PAGE_NUM){
-//                    haveNextPage = false;
-//                }
-//                pageNum++;
-//            }
-//            TransactionTemplate template = new TransactionTemplate(transactionManager);
-//            template.execute(new TransactionCallbackWithoutResult(){
-//                @Override
-//                protected void doInTransactionWithoutResult(TransactionStatus arg0) {
-//                    CloudEventSyncLog cloudEventSyncLog = new CloudEventSyncLog();
-//                    cloudEventSyncLog.setId(logId);
-//                    if(result.size()>0){
-//                        cloudEventSyncLog.setStartTime(result.get(result.size()-1).getEventTime());
-//                        cloudEventSyncLog.setEndTime(result.get(0).getEventTime());
-//                        CloudEventExample cloudEventExample = new CloudEventExample();
-//                        cloudEventExample.createCriteria().andEventTimeBetween(DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",startTime)
-//                                ,DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",endTime));
-//                        cloudEventMapper.deleteByExample(cloudEventExample);
-//                    }else{
-//                        cloudEventSyncLog.setStartTime(DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",startTime));
-//                        cloudEventSyncLog.setEndTime(DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",startTime));
-//                    }
-//                    int num = cloudEventMapper.batchCloudEvents(result);
-//                    cloudEventSyncLog.setDataCount(num);
-//                    cloudEventSyncLog.setStatus(1);
-//                    cloudEventSyncLogMapper.updateByPrimaryKeySelective(cloudEventSyncLog);
-//                }});
-//        }catch (Exception e){
-//            CloudEventSyncLog cloudEventSyncLog = new CloudEventSyncLog();
-//            cloudEventSyncLog.setId(logId);
-//            cloudEventSyncLog.setStatus(2);
-//            cloudEventSyncLog.setException(e.getMessage());
-//            cloudEventSyncLogMapper.updateByPrimaryKeySelective(cloudEventSyncLog);
-//        }
-//    }
-//
-//    public List<CloudEvent>  getCloudEvents(String accountId, String region, String startTime, String endTime,
-//                                              int pageNum, int maxResult) throws Exception {
-//        AccountWithBLOBs account = accountService.getAccount(accountId);
-//        Map<String, String> accountMap = PlatformUtils.getAccount(account, region, proxyMapper.selectByPrimaryKey(account.getProxyId()));
-//        return getCloudEvents(account,accountMap,startTime,endTime,pageNum,maxResult);
-//    }
-//
-//    public List<CloudEvent>  getCloudEvents(AccountWithBLOBs account,Map<String, String> accountMap,String startTime, String endTime,
-//                                               int pageNum, int maxResult) throws Exception {
-//        List<CloudEvent> result;
-//        switch (account.getPluginId()){
-//            case PlatformUtils.aliyun:
-//                result = getAliyunCloudEvents(accountMap,startTime,endTime,pageNum,maxResult);
-//                break;
-//            case PlatformUtils.huawei:
-//                result = getHuaweiCloudEvents(accountMap,startTime,endTime,pageNum,maxResult);
-//            default:
-//                throw new IllegalStateException("Unexpected value: " + account.getPluginId());
-//        }
-//        return result;
-//    }
-//
-//    private List<CloudEvent> getHuaweiCloudEvents(Map<String,String> accountMap,String startTime
-//            , String endTime,int pageNum,int maxResult) {
-//        return new ArrayList<>();
-//    }
-//
-//    private List<CloudEvent> getTencentEvents(Map<String,String> accountMap,String startTime
-//            , String endTime,int pageNum,int maxResult) {
-//        return new ArrayList<>();
-//    }
-//    public List<CloudEvent> getAliyunCloudEvents(Map<String, String> accountMap, String startTime, String endTime
-//            , int pageNum, int maxResult) throws Exception {
-//        Config config = new Config().setAccessKeyId(accountMap.get("accessKey")).setAccessKeySecret(accountMap.get("secretKey"));
-//        config.endpoint = "actiontrail."+accountMap.get("region")+".aliyuncs.com";
-//        Client client = new Client(config);
-//        LookupEventsRequest lookupEventsRequest = new LookupEventsRequest();
-//        lookupEventsRequest.setStartTime(startTime);
-//        lookupEventsRequest.setEndTime(endTime);
-//        lookupEventsRequest.setNextToken((pageNum-1)*maxResult+"");
-//        lookupEventsRequest.setMaxResults(maxResult+"");
-//        lookupEventsRequest.setEventRW("All");
-//        LookupEventsResponse lookupEventsResponse = client.lookupEvents(lookupEventsRequest);
-//        List<Map<String, ?>> events = lookupEventsResponse.body.getEvents();
-//        return events.stream().map(item -> {
-//            return CloudEvent.builder().eventId((String)item.get("eventId"))
-//                    .eventName((String)item.get("eventName")).eventRw((String)item.get("eventRw"))
-//                    .eventTime(DateUtils.dateTime("yyyy-MM-dd HH:mm:ss",(String)item.get("eventTime")))
-//                    .eventMessage((String)item.get("eventMessage")).eventSource((String)item.get("eventSource"))
-//                    .acsRegion((String)item.get("acsRegion")).userAgent((String)item.get("userAgent"))
-//                    .sourceIpAddress((String) item.get("sourceIpAddress")).serviceName((String) item.get("serviceName")).build();
-//
-//        }).collect(Collectors.toList());
-//    }
+    public List<CloudEvent> getCloudEvents(String accountId,String region,String startTime,String endTime){
+        CloudEventExample cloudEventExample = new CloudEventExample();
+        cloudEventExample.createCriteria().andCloudAccountIdEqualTo(accountId).andSyncRegionEqualTo(region)
+                .andEventTimeBetween(DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",startTime),DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",endTime));
+        return cloudEventMapper.selectByExample(cloudEventExample);
+    }
+    public void syncCloudEvents(String accountId,String region,String startTime,String endTime){
+        CloudEventSyncLog cloudEventSyncLog = new CloudEventSyncLog();
+        cloudEventSyncLog.setAccountId(accountId);
+        cloudEventSyncLog.setRegion(region);
+        cloudEventSyncLog.setCreateTime(DateUtils.getNowDate());
+        cloudEventSyncLog.setRequestStartTime(DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",startTime));
+        cloudEventSyncLog.setRequestEndTime(DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",endTime));
+        cloudEventSyncLog.setStatus(0);
+        CloudEventSyncLogExample cloudEventSyncLogExample = new CloudEventSyncLogExample();
+        cloudEventSyncLogExample.createCriteria().andStatusEqualTo(0).andAccountIdEqualTo(accountId)
+                .andRegionEqualTo(region);
+        List<CloudEventSyncLog> cloudEventSyncLogs = cloudEventSyncLogMapper.selectByExample(cloudEventSyncLogExample);
+        if(cloudEventSyncLogs.size()>0){
+            throw new RuntimeException("task running!");
+        }
+        cloudEventSyncLogMapper.insertSelective(cloudEventSyncLog);
+        commonThreadPool.addTask(() -> {
+            saveCloudEvent(cloudEventSyncLog.getId(),accountId,region,startTime,endTime);
+        });
+    }
+
+    public void saveCloudEvent(Integer logId,String accountId, String region, String startTime, String endTime){
+        try{
+            AccountWithBLOBs account = accountService.getAccount(accountId);
+            Map<String, String> accountMap = PlatformUtils.getAccount(account, region, proxyMapper.selectByPrimaryKey(account.getProxyId()));
+            List<CloudEvent> result = new ArrayList<>();
+            int pageNum = 1;
+            int maxNum = 50;
+            boolean haveNextPage = true;
+            while (haveNextPage){
+                List<CloudEvent> pageResult = getCloudEvents(account,accountMap,startTime,endTime,pageNum,maxNum);
+                result.addAll(pageResult);
+                if(pageResult.size() < maxNum || pageNum > MAX_PAGE_NUM){
+                    haveNextPage = false;
+                }
+                pageNum++;
+            }
+            TransactionTemplate template = new TransactionTemplate(transactionManager);
+            template.execute(new TransactionCallbackWithoutResult(){
+                @Override
+                protected void doInTransactionWithoutResult(TransactionStatus arg0) {
+                    CloudEventSyncLog cloudEventSyncLog = new CloudEventSyncLog();
+                    cloudEventSyncLog.setId(logId);
+                    if(result.size()>0){
+                        cloudEventSyncLog.setStartTime(result.get(result.size()-1).getEventTime());
+                        cloudEventSyncLog.setEndTime(result.get(0).getEventTime());
+                        CloudEventExample cloudEventExample = new CloudEventExample();
+                        cloudEventExample.createCriteria().andEventTimeBetween(DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",startTime)
+                                ,DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",endTime));
+                        cloudEventMapper.deleteByExample(cloudEventExample);
+                    }else{
+                        cloudEventSyncLog.setStartTime(DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",startTime));
+                        cloudEventSyncLog.setEndTime(DateUtils.dateTime("yyyy-MM-dd HH:mi:ss",startTime));
+                    }
+                    int num = cloudEventMapper.batchCloudEvents(result);
+                    cloudEventSyncLog.setDataCount(num);
+                    cloudEventSyncLog.setStatus(1);
+                    cloudEventSyncLogMapper.updateByPrimaryKeySelective(cloudEventSyncLog);
+                }});
+        }catch (Exception e){
+            CloudEventSyncLog cloudEventSyncLog = new CloudEventSyncLog();
+            cloudEventSyncLog.setId(logId);
+            cloudEventSyncLog.setStatus(2);
+            cloudEventSyncLog.setException(e.getMessage());
+            cloudEventSyncLogMapper.updateByPrimaryKeySelective(cloudEventSyncLog);
+        }
+    }
+
+    public List<CloudEvent>  getCloudEvents(String accountId, String region, String startTime, String endTime,
+                                              int pageNum, int maxResult) throws Exception {
+        AccountWithBLOBs account = accountService.getAccount(accountId);
+        Map<String, String> accountMap = PlatformUtils.getAccount(account, region, proxyMapper.selectByPrimaryKey(account.getProxyId()));
+        return getCloudEvents(account,accountMap,startTime,endTime,pageNum,maxResult);
+    }
+
+    public List<CloudEvent>  getCloudEvents(AccountWithBLOBs account,Map<String, String> accountMap,String startTime, String endTime,
+                                               int pageNum, int maxResult) throws Exception {
+        List<CloudEvent> result;
+        switch (account.getPluginId()){
+            case PlatformUtils.aliyun:
+                result = getAliyunCloudEvents(accountMap,startTime,endTime,pageNum,maxResult);
+                break;
+            case PlatformUtils.huawei:
+                result = getHuaweiCloudEvents(accountMap,startTime,endTime,pageNum,maxResult);
+            default:
+                throw new IllegalStateException("Unexpected value: " + account.getPluginId());
+        }
+        return result;
+    }
+
+    private List<CloudEvent> getHuaweiCloudEvents(Map<String,String> accountMap,String startTime
+            , String endTime,int pageNum,int maxResult) {
+        return new ArrayList<>();
+    }
+
+    private List<CloudEvent> getTencentEvents(Map<String,String> accountMap,String startTime
+            , String endTime,int pageNum,int maxResult) {
+        return new ArrayList<>();
+    }
+    public List<CloudEvent> getAliyunCloudEvents(Map<String, String> accountMap, String startTime, String endTime
+            , int pageNum, int maxResult) throws Exception {
+        Config config = new Config().setAccessKeyId(accountMap.get("accessKey")).setAccessKeySecret(accountMap.get("secretKey"));
+        config.endpoint = "actiontrail."+accountMap.get("region")+".aliyuncs.com";
+        Client client = new Client(config);
+        LookupEventsRequest lookupEventsRequest = new LookupEventsRequest();
+        lookupEventsRequest.setStartTime(startTime);
+        lookupEventsRequest.setEndTime(endTime);
+        lookupEventsRequest.setNextToken((pageNum-1)*maxResult+"");
+        lookupEventsRequest.setMaxResults(maxResult+"");
+        lookupEventsRequest.setEventRW("All");
+        LookupEventsResponse lookupEventsResponse = client.lookupEvents(lookupEventsRequest);
+        List<Map<String, ?>> events = lookupEventsResponse.body.getEvents();
+        return events.stream().map(item -> {
+            return CloudEvent.builder().eventId((String)item.get("eventId"))
+                    .eventName((String)item.get("eventName")).eventRw((String)item.get("eventRw"))
+                    .eventTime(DateUtils.dateTime("yyyy-MM-dd HH:mm:ss",(String)item.get("eventTime")))
+                    .eventMessage((String)item.get("eventMessage")).eventSource((String)item.get("eventSource"))
+                    .acsRegion((String)item.get("acsRegion")).userAgent((String)item.get("userAgent"))
+                    .sourceIpAddress((String) item.get("sourceIpAddress")).serviceName((String) item.get("serviceName")).build();
+
+        }).collect(Collectors.toList());
+    }
 
 }
