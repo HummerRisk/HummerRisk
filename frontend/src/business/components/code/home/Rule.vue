cea728dcb90b078a717951bbe96a66c4f5672775
harris <ma402847591@gmail.com>
2022-08-26 06:14:44 +0800

    feat(code): code前端

---
+++ frontend/src/business/components/code/home/Rule.vue
@@ -0,0 +1,50 @@
+<template>
+  <main-container>
+  </main-container>
+</template>
+
+<script>
+
+/* eslint-disable */
+export default {
+}
+</script>
+
+<style scoped>
+.table-content {
+  width: 100%;
+}
+
+.demo-table-expand {
+  font-size: 0;
+}
+.demo-table-expand label {
+  width: 90px;
+  color: #99a9bf;
+}
+.demo-table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  padding: 10px 10%;
+  width: 47%;
+}
+.tag-v{
+  margin: 10px;
+  cursor:pointer;
+}
+
+.rtl >>> .el-drawer__body {
+  overflow-y: auto;
+  padding: 20px;
+}
+.rtl >>> input {
+  width: 100%;
+}
+.rtl >>> .el-select {
+  width: 80%;
+}
+.rtl >>> .el-form-item__content {
+  width: 75%;
+}
+/deep/ :focus{outline:0;}
+</style>
