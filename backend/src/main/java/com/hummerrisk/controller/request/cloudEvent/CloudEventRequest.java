dfd3deaeff91fd1827769c9a0f20c701ab06c12e
wangjiankun233 <wjk5442908@163.com>
2022-08-27 00:52:33 +0800

    feat(cloudEvent):修改增加UTC时间与本地时间的转换

---
+++ backend/src/main/java/com/hummerrisk/controller/request/cloudEvent/CloudEventRequest.java
@@ -0,0 +1,40 @@
+package com.hummerrisk.controller.request.cloudEvent;
+
+public class CloudEventRequest {
+    private String accountId;
+    private String region;
+    private String startTime;
+    private String endTime;
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+}
