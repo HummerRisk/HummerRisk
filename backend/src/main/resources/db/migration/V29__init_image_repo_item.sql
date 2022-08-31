27e8dfa4943661b0c96599f49669c074dc0b41eb
harris <ma402847591@gmail.com>
2022-08-31 21:31:23 +0800

    refactor(sql): 添加image repo

--- backend/src/main/resources/db/migration/V29__init_image_repo_item.sql
+++ backend/src/main/resources/db/migration/V29__init_image_repo_item.sql
@@ -2,12 +2,12 @@ CREATE TABLE IF NOT EXISTS `image_repo_item` (
     `id`                         varchar(50)        NOT NULL COMMENT 'ID',
     `repo_id`                    varchar(50)        DEFAULT NULL COMMENT '镜像仓库ID',
     `digest`                     varchar(256)       DEFAULT NULL COMMENT 'digest',
-    `project`                    varchar(32)        DEFAULT NULL COMMENT 'project',
-    `repository`                 varchar(32)        DEFAULT NULL COMMENT 'repository',
-    `tag`                        varchar(32)        DEFAULT NULL COMMENT 'tag',
+    `project`                    varchar(128)       DEFAULT NULL COMMENT 'project',
+    `repository`                 varchar(128)       DEFAULT NULL COMMENT 'repository',
+    `tag`                        varchar(50)        DEFAULT NULL COMMENT 'tag',
     `path`                       varchar(256)       DEFAULT NULL COMMENT '镜像地址',
-    `size`											 bigint(15)					DEFAULT NULL COMMENT '大小',
-    `arch`             					 varchar(64)        DEFAULT NULL COMMENT 'cpu架构',
+    `size`						 varchar(50)		DEFAULT NULL COMMENT '大小',
+    `arch`             			 varchar(64)        DEFAULT NULL COMMENT 'cpu架构',
     `push_time`                  bigint(15)         DEFAULT NULL COMMENT '推送时间',
     PRIMARY KEY (`id`)
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
