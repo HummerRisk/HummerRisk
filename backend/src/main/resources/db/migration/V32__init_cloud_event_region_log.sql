5cbc9ea48f2f5c4032a691998251f243e798d9ec
harris <ma402847591@gmail.com>
2022-09-13 06:03:24 +0800

    refactor(sql): 优化sql

--- backend/src/main/resources/db/migration/V32__init_cloud_event_region_log.sql
+++ backend/src/main/resources/db/migration/V32__init_cloud_event_region_log.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `cloud_event_region_log` (
       `id` int(8) NOT NULL AUTO_INCREMENT COMMENT 'id',
       `log_id` int(8) DEFAULT NULL COMMENT '同步日志ID',
-      `region` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '区域',
+      `region` varchar(512) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '区域',
       `data_count` int(6) DEFAULT NULL COMMENT '同步数据量',
       `status` int(1) DEFAULT NULL COMMENT '状态：0 同步中，1成功，2失败',
       `start_time` datetime DEFAULT NULL COMMENT '日志实际开始时间',
