1b18fe3b1e51475312a11c2f5c826195d088ba7c
harris <ma402847591@gmail.com>
2022-09-22 07:01:38 +0800

    refactor(result): 优化检测结果

--- frontend/src/business/components/server/home/ServerList.vue
+++ frontend/src/business/components/server/home/ServerList.vue
@@ -604,12 +604,11 @@ import ServerKeyUpload from "@/business/components/server/head/ServerKeyUpload";
       },
       saveServer(servers) {
         for (let server of servers) {
-          console.log(server)
           if(!server.name || !server.ip || !server.userName || !server.groupId) {
             this.$warning('Value will not be null');
             return;
           } else {
-            if (server.isPublicKey !== "no") {
+            if (!server.isCertificate && server.isPublicKey !== "no") {
               if(!server.password) this.$warning('Password will not be null');
               return;
             }
