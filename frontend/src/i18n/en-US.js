c8ee64a63c73c57b45ef80d4425ebd0a0e60df05
wangjiankun233 <wjk5442908@163.com>
2022-10-07 01:05:15 +0800

    feat(i18n):新增资源同步翻译

--- frontend/src/i18n/en-US.js
+++ frontend/src/i18n/en-US.js
@@ -621,6 +621,7 @@ export default {
     rule_group: 'Cloud detection rule group',
   },
   resource: {
+    sync_log: 'Resource synchronization log',
     resource_id: 'resourceId',
     statistics:'Compliance Report',
     cloud_statistics: 'Cloud resource compliance report',
