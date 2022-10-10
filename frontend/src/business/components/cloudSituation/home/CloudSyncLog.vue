854d9665c4cfd06a179e591341366fa959746fb9
wangjiankun233 <wjk5442908@163.com>
2022-10-10 12:13:16 +0800

    feat(resource):修改资源列表

--- frontend/src/business/components/cloudSituation/home/CloudSyncLog.vue
+++ frontend/src/business/components/cloudSituation/home/CloudSyncLog.vue
@@ -240,7 +240,7 @@ export default {
       this.createVisible = false;
     },
     handleDelete(obj) {
-      this.$alert(this.$t('workspace.delete_confirm') + this.$t('resource.sync_log') + " ？", '', {
+      this.$alert(this.$t('workspace.delete_confirm'), '', {
         confirmButtonText: this.$t('commons.confirm'),
         callback: (action) => {
           if (action === 'confirm') {
