af18dcd98e0c0c7a56d35391ed1aed2294677c44
harris <ma402847591@gmail.com>
2022-10-20 01:21:17 +0800

    refactor(dashboard): 优化首页图表展示

--- frontend/src/business/components/dashboard/head/ServerChart.vue
+++ frontend/src/business/components/dashboard/head/ServerChart.vue
@@ -5,14 +5,12 @@
 </template>
 
 <script>
-import echarts from 'echarts';
 import HrChart from "@/business/components/common/chart/HrChart";
 /* eslint-disable */
 export default {
   name: "ServerChart",
   components: {
     HrChart,
-    echarts,
   },
   props: {
     data: {},
@@ -24,129 +22,39 @@ export default {
   },
   methods: {
     init() {
-      this.options = {
-        series: [
-          {
-            type: 'treemap',
-            data: [
-              {
-                name: this.$t('server.server_1'),
-                value: 10,
-                children: [
-                  {
-                    name: this.$t('server.server1'),
-                    value: 5
-                  },
-                  {
-                    name: this.$t('server.server2'),
-                    value: 5
-                  }
-                ]
-              },
-              {
-                name: this.$t('server.server_2'),
-                value: 20,
-                children: [
-                  {
-                    name: this.$t('server.server_2'),
-                    value: 10,
-                    children: [
-                      {
-                        name: this.$t('server.server3'),
-                        value: 20
-                      },
-                      {
-                        name: this.$t('server.server4'),
-                        value: 20
-                      }
-                    ]
-                  }
-                ]
-              },
-              {
-                name: this.$t('server.server_3'),
-                value: 20,
-                children: [
-                  {
-                    name: this.$t('server.server_3'),
-                    value: 20,
-                    children: [
-                      {
-                        name: this.$t('server.server5'),
-                        value: 20
-                      },
-                      {
-                        name: this.$t('server.server6'),
-                        value: 20
-                      }
-                    ]
-                  }
-                ]
-              },
-              {
-                name: this.$t('server.server_4'),
-                value: 20,
-                children: [
-                  {
-                    name: this.$t('server.server_4'),
-                    value: 20,
-                    children: [
-                      {
-                        name: this.$t('server.server7'),
-                        value: 20
-                      }
-                    ]
-                  }
-                ]
-              },
-              {
-                name: this.$t('server.server_5'),
-                value: 20,
-                children: [
-                  {
-                    name: this.$t('server.server_5'),
-                    value: 20,
-                    children: [
-                      {
-                        name: this.$t('server.server8'),
-                        value: 20
-                      }
-                    ]
-                  }
-                ]
-              },
-              {
-                name: this.$t('server.server_6'),
-                value: 20,
-                children: [
-                  {
-                    name: this.$t('server.server_6'),
-                    value: 20,
-                    children: [
-                      {
-                        name: this.$t('server.server9'),
-                        value: 20
-                      },
-                      {
-                        name: this.$t('server.server10'),
-                        value: 20
-                      }
-                    ]
-                  }
-                ]
+      this.$get("/server/serverChart", response => {
+        let data = response.data;
+        this.options = {
+          title: {
+            text: this.$t('server.server_risk_chart'),
+            subtext: this.$t('server.server_risk_chart_vuln'),
+            left: 'center'
+          },
+          tooltip: {
+            trigger: 'item'
+          },
+          legend: {
+            orient: 'vertical',
+            left: 'left'
+          },
+          series: [
+            {
+              name: 'Server',
+              type: 'pie',
+              radius: '50%',
+              data: data,
+              emphasis: {
+                itemStyle: {
+                  shadowBlur: 10,
+                  shadowOffsetX: 0,
+                  shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
               }
-            ]
-          }
-        ],
-        grid: {
-          top: '10%',
-          left: '1%',
-          right: '2%',
-          bottom: '2%',
-          containLabel: true
-        },
-        color: ['#11cfae', '#009ef0', '#627dec', '#893fdc', '#1eb27f','#0051a4']
-      };
+            }
+          ],
+          color: ['#11cfae', '#009ef0', '#627dec', '#893fdc', '#89ffff','#0051a4', '#8B0000', '#FF4D4D', '#FF8000', '#336D9F']
+        };
+      });
     },
   },
   created() {
