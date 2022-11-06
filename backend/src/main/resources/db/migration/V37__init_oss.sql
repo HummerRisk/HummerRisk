36d5ed674d4d02ede29d2812136cc0d5d4ac2d4b
harris <ma402847591@gmail.com>
2022-11-07 04:35:43 +0800

    refactor(oss): 添加阿里云bucket

--- backend/src/main/resources/db/migration/V37__init_oss.sql
+++ backend/src/main/resources/db/migration/V37__init_oss.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS `oss`
     `id`                         varchar(50)         NOT NULL COMMENT 'ID',
     `name`                       varchar(128)        DEFAULT NULL UNIQUE COMMENT '云账号名称',
     `credential`                 longtext            DEFAULT NULL COMMENT '云账号凭证',
+    `endpoint`                   varchar(256)        DEFAULT NULL COMMENT 'Endpoint',
     `plugin_id`                  varchar(50)         DEFAULT NULL COMMENT '插件ID',
     `plugin_name`                varchar(100)        DEFAULT NULL COMMENT '插件名称',
     `plugin_icon`                varchar(256)        DEFAULT NULL COMMENT '插件图标',
@@ -16,28 +17,9 @@ CREATE TABLE IF NOT EXISTS `oss`
     PRIMARY KEY (`id`)
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `oss_item`
-(
-    `id`                         varchar(50)         NOT NULL COMMENT 'ID',
-    `name`                       varchar(128)        DEFAULT NULL COMMENT '云账号名称',
-    `oss_id`                     varchar(50)         DEFAULT NULL COMMENT 'Oss ID',
-    `plugin_id`                  varchar(50)         DEFAULT NULL COMMENT '插件ID',
-    `plugin_name`                varchar(100)        DEFAULT NULL COMMENT '插件名称',
-    `plugin_icon`                varchar(256)        DEFAULT NULL COMMENT '插件图标',
-    `status`                     varchar(10)         DEFAULT NULL COMMENT '同步状态',
-    `create_time`                bigint(13)          DEFAULT NULL COMMENT '创建时间',
-    `update_time`                bigint(13)          DEFAULT NULL COMMENT '更新时间',
-    `creator`                    varchar(128)        DEFAULT NULL COMMENT '创建人',
-    `region_id`                  varchar(100)        DEFAULT NULL COMMENT '区域ID',
-    `region_name`                varchar(100)        DEFAULT NULL COMMENT '区域名称',
-    `proxy_id`                   int(11)             DEFAULT NULL COMMENT '代理ID',
-    PRIMARY KEY (`id`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
-
-CREATE TABLE IF NOT EXISTS `oss_item_log` (
+CREATE TABLE IF NOT EXISTS `oss_log` (
     `id`                           int(11)             NOT NULL AUTO_INCREMENT,
     `oss_id`                       varchar(50)         DEFAULT NULL COMMENT 'Oss ID',
-    `oss_item_id`                  varchar(50)         DEFAULT NULL COMMENT 'Oss item ID',
     `region_id`                    varchar(100)        DEFAULT NULL COMMENT '区域ID',
     `region_name`                  varchar(100)        DEFAULT NULL COMMENT '区域名称',
     `create_time`                  bigint(13)          DEFAULT NULL COMMENT '创建时间',
@@ -46,7 +28,9 @@ CREATE TABLE IF NOT EXISTS `oss_item_log` (
     `output`                       mediumtext          DEFAULT NULL COMMENT '输出',
     `result`                       tinyint(1)          DEFAULT NULL COMMENT '结果',
     PRIMARY KEY (`id`)
-    ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAUCREATE TABLE IF NOT EXISTS `oss_bucket` (
+    ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `oss_bucket` (
     `id`                           varchar(50)         NOT NULL COMMENT 'ID',
     `bucket_name`                  varchar(64)         DEFAULT NULL COMMENT '存储名称',
     `storage_class`                varchar(64)         DEFAULT NULL COMMENT '存储类型',
@@ -55,15 +39,16 @@ CREATE TABLE IF NOT EXISTS `oss_item_log` (
     `intranet_endpoint`            varchar(256)        DEFAULT NULL COMMENT '内部节点',
     `extranet_endpoint`            varchar(256)        DEFAULT NULL COMMENT '外部节点',
     `oss_id`                       varchar(50)         DEFAULT NULL COMMENT 'Oss ID',
-    `oss_item_id`                  varchar(50)         DEFAULT NULL COMMENT 'Oss item ID',
+    `owner_id`                     varchar(80)         DEFAULT NULL COMMENT '所有者id',
+    `owner_name`                   varchar(80)         DEFAULT NULL COMMENT '所有者',
     `sync_status`                  varchar(50)         DEFAULT NULL COMMENT '同步状态',
     `domain_name`                  varchar(500)        DEFAULT NULL COMMENT '存储桶域名',
     `size`                         bigint(20)          COLLATE utf8mb4_bin DEFAULT NULL COMMENT '存储用量(M)',
     `object_number`                bigint(20)          COLLATE utf8mb4_bin COMMENT '文件数量',
     `canned_acl`                   varchar(45)         DEFAULT NULL COMMENT '权限',
+    `sync_flag`                    tinyint(1)          DEFAULT 0 COMMENT '是否启用',
     PRIMARY KEY (`id`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;LT CHARSET = utf8mb4;
-
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 
 ALTER TABLE `cloud_native_source` ADD `source_json` longtext DEFAULT NULL COMMENT '资源json';
