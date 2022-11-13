9d830834940b66ea9a609ca9c420b706006750c4
harris <ma402847591@gmail.com>
2022-11-13 23:20:15 +0800

    refactor(baidu): 对象存储添加百度云

--- backend/src/main/java/com/hummerrisk/oss/provider/BaiduProvider.java
+++ backend/src/main/java/com/hummerrisk/oss/provider/BaiduProvider.java
@@ -1,10 +1,24 @@
 package com.hummerrisk.oss.provider;
 
+import com.alibaba.fastjson.JSON;
+import com.baidubce.services.bos.BosClient;
+import com.baidubce.services.bos.model.*;
 import com.hummerrisk.base.domain.OssBucket;
 import com.hummerrisk.base.domain.OssWithBLOBs;
+import com.hummerrisk.commons.exception.PluginException;
+import com.hummerrisk.commons.utils.LogUtil;
+import com.hummerrisk.oss.constants.ObjectTypeConstants;
 import com.hummerrisk.oss.dto.BucketObjectDTO;
+import com.hummerrisk.proxy.baidu.BaiduCredential;
+import com.hummerrisk.proxy.baidu.BaiduRequest;
+import com.hummerrisk.service.SysListener;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
 
+import java.io.BufferedInputStream;
 import java.io.FilterInputStream;
+import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,21 +30,216 @@ public class BaiduProvider implements OssProvider {
     }
 
     @Override
-    public List<OssBucket> getOssBucketList(OssWithBLOBs ossAccount) {
+    public List<OssBucket> getOssBucketList(OssWithBLOBs ossAccount) throws Exception {
         List<OssBucket> resultList = new ArrayList<>();
+        try {
+            BosClient bosClient = getBosClient(ossAccount);
+            ListBucketsResponse list = bosClient.listBuckets();
+            if (!CollectionUtils.isEmpty(list.getBuckets())) {
+                for (BucketSummary bucketSummary : list.getBuckets()) {
+                    resultList.add(setBucket(bosClient, ossAccount, bucketSummary, null));
+                }
+            }
+            bosClient.shutdown();
+        } catch (PluginException e) {
+            throw new PluginException(e.getMessage());
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+
         return resultList;
     }
 
+    private static OssBucket setBucket(BosClient bosClient, OssWithBLOBs account, BucketSummary bucketSummary, OssBucket bucket) throws Exception {
+        OssBucket bucketDTO = new OssBucket();
+        String bucketName = bucketSummary.getName();
+        ListObjectsResponse listObjectsResponse = bosClient.listObjects(bucketName);
+        List<BosObjectSummary> contents = listObjectsResponse.getContents();
+        try {
+            bucketDTO.setCreateTime(bucketSummary.getCreationDate().getTime());
+            bucketDTO.setLocation(bosClient.getBucketLocation(bucketName).getLocationConstraint());
+            bucketDTO.setBucketName(bucketName);
+            bucketDTO.setOssId(account.getId());
+            bucketDTO.setDomainName(StringUtils.joinWith(".", bucketName, bosClient.getBucketLocation(bucketName).getLocationConstraint(), "bcebos.com"));
+            bucketDTO.setExtranetEndpoint(StringUtils.joinWith(".", bosClient.getBucketLocation(bucketName).getLocationConstraint(), "bcebos.com"));
+            bucketDTO.setIntranetEndpoint("N/A");
+            bucketDTO.setCannedAcl(chooseAccessControlList(bucket != null ? bucket.getCannedAcl() : "N/A"));
+            if (contents.size() > 0) {
+                bucketDTO.setStorageClass(contents.get(0).getStorageClass());
+            } else {
+                bucketDTO.setStorageClass(chooseStorageClassEnum(bucket != null ? bucket.getStorageClass() : "N/A"));
+            }
+            if (bosClient.getBosAccountOwner() != null) {
+                bucketDTO.setOwnerName(bosClient.getBosAccountOwner().getDisplayName());
+                bucketDTO.setOwnerId(bosClient.getBosAccountOwner().getId());
+            }
+            if (listObjectsResponse != null) {
+                Double size = 0D;
+                for (BosObjectSummary obj : contents) {
+                    size += obj.getSize();
+                }
+                bucketDTO.setSize(SysListener.changeFlowFormat(size.longValue()));
+                bucketDTO.setObjectNumber((long) contents.size() - 1);
+            } else {
+                bucketDTO.setSize("0");
+                bucketDTO.setObjectNumber(0L);
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+        return bucketDTO;
+    }
+
+    private static String chooseStorageClassEnum(String storageClass) {
+        // 类型不确定，先这么搁置
+        if (StringUtils.equals(storageClass, "STANDARD")) {
+            return "STANDARD";
+        } else if (StringUtils.equals(storageClass, "STANDARD")) {
+            return "STANDARD";
+        } else if (StringUtils.equals(storageClass, "COLD")) {
+            return "COLD";
+        } else if (StringUtils.equals(storageClass, "ARCHIVE")) {
+            return "ARCHIVE";
+        }
+        return "STANDARD";
+    }
+
+    private static String chooseAccessControlList(String cannedAcl) {
+        if (StringUtils.equals(cannedAcl, CannedAccessControlList.Private.toString())) {
+            return CannedAccessControlList.Private.name();
+        } else if (StringUtils.equals(cannedAcl, CannedAccessControlList.PublicRead.toString())) {
+            return CannedAccessControlList.PublicRead.name();
+        } else if (StringUtils.equals(cannedAcl, CannedAccessControlList.PublicReadWrite.toString())) {
+            return CannedAccessControlList.PublicReadWrite.name();
+        }
+        return "N/A";
+    }
+
+    private static BosClient getBosClient(OssWithBLOBs account) throws Exception {
+        BaiduCredential baiduCredential = JSON.parseObject(account.getCredential(), BaiduCredential.class);
+        BaiduRequest req = new BaiduRequest();
+        req.setCredential(JSON.toJSONString(baiduCredential));
+        return req.getClient();
+    }
+
     @Override
-    public List<BucketObjectDTO> getBucketObjects(OssBucket bucket, OssWithBLOBs account, String prefix) {
+    public List<BucketObjectDTO> getBucketObjects(OssBucket bucket, OssWithBLOBs account, String prefix) throws Exception {
+        try {
+            BosClient bosClient = getBosClient(account);
+            if (bosClient.doesBucketExist(bucket.getBucketName())) {
+                ListObjectsRequest listObjectsRequest = new ListObjectsRequest(bucket.getBucketName());
+                listObjectsRequest.setBucketName(bucket.getBucketName());
+                listObjectsRequest.setMaxKeys(1000);
+                listObjectsRequest.setDelimiter("/");
+                if (StringUtils.isNotEmpty(prefix)) {
+                    listObjectsRequest.setPrefix(prefix);
+                }
+                List<BucketObjectDTO> objects = new ArrayList<>();
+
+                ListObjectsResponse objectListing = bosClient.listObjects(listObjectsRequest);
+                objects.addAll(convertToBucketFolder(bucket, objectListing.getCommonPrefixes(), prefix));
+                objects.addAll(convertToBucketObject(bosClient, bucket, objectListing.getContents(), prefix));
+
+                while (objectListing.getNextMarker() != null) {
+                    objectListing = bosClient.listObjects(listObjectsRequest);
+                    objects.addAll(convertToBucketObject(bosClient, bucket, objectListing.getContents(), prefix));
+                    objects.addAll(convertToBucketFolder(bucket, objectListing.getCommonPrefixes(), prefix));
+                }
+                bosClient.shutdown();
+                List<BucketObjectDTO> bucketObjectDTOS = new ArrayList<>();
+                for (BucketObjectDTO object : objects) {
+                    if (object.getObjectType().equals(ObjectTypeConstants.BACK.name())) {
+                        bucketObjectDTOS.add(0, object);
+                    } else {
+                        bucketObjectDTOS.add(object);
+                    }
+                }
+                return bucketObjectDTOS;
+            } else {
+                throw new Exception("no such bucket");
+            }
+        } catch (Exception e) {
+            LogUtil.error(e.getMessage(), e);
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    private List<BucketObjectDTO> convertToBucketFolder(OssBucket bucket, List<String> commonPrefixes, String prefix) {
         List<BucketObjectDTO> objects = new ArrayList<>();
+        for (String commonPrefix : commonPrefixes) {
+            BucketObjectDTO bucketObject = new BucketObjectDTO();
+            bucketObject.setBucketId(bucket.getId());
+            if (StringUtils.isNotEmpty(prefix)) {
+                bucketObject.setObjectName(commonPrefix.substring(prefix.length(), commonPrefix.length()));
+            } else {
+                bucketObject.setObjectName(commonPrefix);
+            }
+            bucketObject.setId(commonPrefix);
+            bucketObject.setObjectType(ObjectTypeConstants.DIR.name());
+            objects.add(bucketObject);
+        }
         return objects;
+    }
+
+    private List<BucketObjectDTO> convertToBucketObject(BosClient bosClient, OssBucket bucket, List<BosObjectSummary> bosObjectSummaryList, String prefix) {
+        List<BucketObjectDTO> objects = new ArrayList<>();
+        for (BosObjectSummary bosObjectSummary : bosObjectSummaryList) {
+            BucketObjectDTO bucketObject = new BucketObjectDTO();
+            bucketObject.setBucketId(bucket.getId());
+
+            if (StringUtils.isNotEmpty(prefix) && bosObjectSummary.getKey().equals(prefix)) {
+                String[] dirs = prefix.split("/");
+                if (dirs.length == 1) {
+                    bucketObject.setId("/");
+                    bucketObject.setObjectName(prefix);
+                } else {
+                    String lastDir = dirs[dirs.length - 1];
+                    bucketObject.setId(prefix.substring(0, prefix.length() - lastDir.length() - 1));
+                    bucketObject.setObjectName(lastDir + "/");
+                }
+                bucketObject.setObjectType(ObjectTypeConstants.BACK.name());
+            }
+
+            if (!bosObjectSummary.getKey().endsWith("/")) {
+                bucketObject.setId(bosObjectSummary.getKey());
+                if (StringUtils.isNotEmpty(prefix)) {
+                    bucketObject.setObjectName(bosObjectSummary.getKey().substring(prefix.length(), bosObjectSummary.getKey().length()));
+                } else {
+                    bucketObject.setObjectName(bosObjectSummary.getKey());
+                }
+                bucketObject.setObjectType(ObjectTypeConstants.FILE.name());
+                long getSize = bosObjectSummary != null ? bosObjectSummary.getSize() : 0;
+                double size = ((double) getSize) / 1024 / 1024;
+                DecimalFormat df = new DecimalFormat("#0.###");
+                bucketObject.setObjectSize(df.format(size));
+                bucketObject.setStorageClass(bosObjectSummary.getStorageClass());
+                bucketObject.setLastModified(bosObjectSummary.getLastModified().getTime());
+                bucketObject.setDownloadUrl(generatePresignedUrl(bosClient, bucket.getBucketName(), bosObjectSummary.getKey()).toString());
+            }
+
+            objects.add(bucketObject);
+        }
+        return objects;
+    }
+
+    private URL generatePresignedUrl(BosClient bosClient, String bucketName, final String objectId) {
+        int expiration = 1800;
+        // 生成以GET方法访问的签名URL，访客可以直接通过浏览器访问相关内容。
+        return bosClient.generatePresignedUrl(bucketName, objectId, expiration);
     }
 
     @Override
     public FilterInputStream downloadObject(OssBucket bucket, OssWithBLOBs account, final String objectId) throws Exception{
-        FilterInputStream filterInputStream = null;
-        return filterInputStream;
+        FilterInputStream input = null;
+        try {
+            BosClient bosClient = getBosClient(account);
+            BosObject obj = bosClient.getObject(bucket.getBucketName(), objectId);
+            input = new BufferedInputStream(obj.getObjectContent());
+            return input;
+        } catch (Exception e) {
+            LogUtil.error(e.getMessage(), e);
+            throw new Exception(e.getMessage());
+        }
     }
 
 }
---
+++ backend/src/main/java/com/hummerrisk/proxy/baidu/BaiduRequest.java
@@ -0,0 +1,106 @@
+package com.hummerrisk.proxy.baidu;
+
+import com.baidubce.auth.DefaultBceCredentials;
+import com.baidubce.services.bos.BosClient;
+import com.baidubce.services.bos.BosClientConfiguration;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.hummerrisk.commons.exception.PluginException;
+import com.hummerrisk.commons.utils.LogUtil;
+import com.hummerrisk.commons.utils.ReadFileUtils;
+import com.hummerrisk.oss.dto.OssRegion;
+import com.hummerrisk.proxy.Request;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BaiduRequest extends Request {
+    private BaiduCredential baiduCredential;
+
+    public BaiduRequest() {
+    }
+
+    public BaiduRequest(Request req) {
+        setCredential(req.getCredential());
+        setRegionId(req.getRegionId());
+    }
+
+    public BaiduCredential getBaiduCredential() {
+        if (baiduCredential == null) {
+            baiduCredential = new Gson().fromJson(getCredential(), BaiduCredential.class);
+        }
+        return baiduCredential;
+    }
+
+    public void setBaiduCredential(BaiduCredential baiduCredential) {
+        this.baiduCredential = baiduCredential;
+        setCredential(new Gson().toJson(baiduCredential));
+    }
+
+    public String getSecretKey() {
+        baiduCredential = getBaiduCredential();
+        if (baiduCredential != null) {
+            return baiduCredential.getSecretAccessKey();
+        }
+        return null;
+    }
+
+    public String getAccessKey() {
+        baiduCredential = getBaiduCredential();
+        if (baiduCredential != null) {
+            return baiduCredential.getAccessKeyId();
+        }
+        return null;
+    }
+
+    public <T> BosClient getClient() throws Exception {
+        BosClient client = null;
+        if (getAccessKey() != null && getAccessKey().trim().length() > 0 && getSecretKey() != null && getSecretKey().trim().length() > 0) {
+
+            BosClientConfiguration config = new BosClientConfiguration();
+
+            // 设置HTTP最大连接数为10
+            config.setMaxConnections(100);
+            // 设置TCP连接超时为5000毫秒
+            config.setConnectionTimeoutInMillis(50000);
+            // 设置Socket传输数据超时的时间为2000毫秒
+            config.setSocketTimeoutInMillis(20000);
+
+            config.setEndpoint(getEndpoint(getRegionId()));
+            config.setCredentials(new DefaultBceCredentials(baiduCredential.getAccessKeyId(), baiduCredential.getSecretAccessKey()));
+            try {
+                client = new BosClient(config);
+            } catch (Exception e) {
+                LogUtil.error(e.getMessage(), e);
+                throw new PluginException(e);
+            }
+        }
+        return client;
+    }
+
+    private String getEndpoint(String region) throws Exception {
+
+        String endpoint = "";
+        // 目前支持“华北-北京”、“华南-广州”和“华东-苏州”三个区域。北京区域：http://bj.bcebos.com，广州区域：http://gz.bcebos.com，苏州区域：http://su.bcebos.com
+        List<OssRegion> regionList = this.getOssRegions();
+        for(OssRegion ossRegion : regionList){
+            if(StringUtils.equalsIgnoreCase(ossRegion.getRegionId(), getRegionId())){
+                endpoint = ossRegion.getExtranetEndpoint();
+                break;
+            }
+        }
+        if(StringUtils.isBlank(endpoint)){
+            // ENDPOINT参数只能用指定的包含区域的域名来进行定义，不指定时默认为北京区域
+            endpoint = "http://bj.bcebos.com";
+        }
+        return endpoint;
+    }
+
+    public List<OssRegion> getOssRegions() throws Exception {
+        String result = ReadFileUtils.readConfigFile("support/regions/", "hummer-baidu-plugin", ".json");
+        return new Gson().fromJson(result, new TypeToken<ArrayList<OssRegion>>() {}.getType());
+    }
+
+}
