2ea06c56e2f4136fe5983170a8687844566e845b
harris <ma402847591@gmail.com>
2022-12-02 04:12:23 +0800

    feat(fs): 依赖文件检测结果

--- backend/src/main/java/com/hummerrisk/base/mapper/ext/ExtFileSystemResultMapper.java
+++ backend/src/main/java/com/hummerrisk/base/mapper/ext/ExtFileSystemResultMapper.java
@@ -3,13 +3,14 @@ package com.hummerrisk.base.mapper.ext;
 import com.hummerrisk.controller.request.fs.FsResultRequest;
 import com.hummerrisk.dto.FsResultDTO;
 import com.hummerrisk.dto.HistoryFsResultDTO;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
 
 public interface ExtFileSystemResultMapper {
 
-    List<FsResultDTO> resultList(FsResultRequest request);
+    List<FsResultDTO> resultList(@Param("request") FsResultRequest request);
 
     FsResultDTO getFsResult(String resultId);
 
