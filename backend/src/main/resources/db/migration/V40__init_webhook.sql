8c793e6bf20dde6a1b5b1e4169ce9001cbbbdd42
harris <ma402847591@gmail.com>
2022-12-09 02:30:51 +0800

    refactor(server): 添加type

--- backend/src/main/resources/db/migration/V40__init_webhook.sql
+++ backend/src/main/resources/db/migration/V40__init_webhook.sql
@@ -15,3 +15,15 @@ CREATE TABLE IF NOT EXISTS `webhook`
 ALTER TABLE `message_task` ADD `webhook_id` varchar(1024) DEFAULT NULL COMMENT 'webhook id';
 
 ALTER TABLE `message_task` ADD `webhook` varchar(1024) DEFAULT NULL COMMENT 'webhook';
+
+ALTER TABLE `server` ADD `type` varchar(64) DEFAULT 'linux' COMMENT 'type: linux/windows';
+
+ALTER TABLE `server_rule` ADD `type` varchar(64) DEFAULT 'linux' COMMENT 'type: linux/windows';
+
+ALTER TABLE `server_result` ADD `type` varchar(64) DEFAULT 'linux' COMMENT 'type: linux/windows';
+
+UPDATE `server` SET `type` = 'linux';
+
+UPDATE `server_rule` SET `type` = 'linux';
+
+UPDATE `server_result` SET `type` = 'linux';
