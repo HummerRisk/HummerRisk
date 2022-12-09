0e762b495ac530bf667d7e6d697f3f1487c14ba8
wangjiankun233 <wjk5442908@163.com>
2022-12-09 22:45:47 +0800

    feat(PlatformUtils):增加京东云

---
+++ backend/src/main/java/com/hummerrisk/proxy/jdcloud/JDCloudCredential.java
@@ -0,0 +1,22 @@
+package com.hummerrisk.proxy.jdcloud;
+
+public class JDCloudCredential {
+    private String accessKey;
+    private String secretAccessKey;
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public String getSecretAccessKey() {
+        return secretAccessKey;
+    }
+
+    public void setSecretAccessKey(String secretAccessKey) {
+        this.secretAccessKey = secretAccessKey;
+    }
+}
