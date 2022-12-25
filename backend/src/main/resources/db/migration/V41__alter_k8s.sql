8515c9102cf5ae72f4d9bde41db7c27e86fb408c
harris <ma402847591@gmail.com>
2022-12-25 22:59:46 +0800

    refactor(sql): kubench item

--- backend/src/main/resources/db/migration/V41__alter_k8s.sql
+++ backend/src/main/resources/db/migration/V41__alter_k8s.sql
@@ -4,4 +4,15 @@ ALTER TABLE `cloud_native` ADD `kubench_status` varchar(10) DEFAULT null COMMENT
 
 ALTER TABLE `cloud_native_result` ADD `kube_bench` longtext DEFAULT null COMMENT 'kube bench';
 
+CREATE TABLE IF NOT EXISTS `cloud_native_result_kubench`
+(
+    `id`                         varchar(50)         NOT NULL,
+    `result_id`                  varchar(50)         DEFAULT NULL COMMENT '云原生检测结果ID',
+    `title`                      longtext            DEFAULT NULL COMMENT 'title',
+    `severity`                   varchar(128)        DEFAULT NULL COMMENT 'severity',
+    `create_time`                bigint(13)          DEFAULT NULL COMMENT '创建时间',
+    `description`                longtext            DEFAULT NULL COMMENT 'description',
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
 
