20b96e1fb2b4089256bb792283644ca9894d01d4
harris <ma402847591@gmail.com>
2023-01-10 17:21:50 +0800

    feat(repo): 新增 image repo setting

---
+++ backend/src/main/resources/db/migration/V42__init_image_repo_setting.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE IF NOT EXISTS `image_repo_setting` (
+    `id`                         varchar(50)        NOT NULL COMMENT 'ID',
+    `repo_id`                    varchar(50)        DEFAULT NULL COMMENT '镜像仓库ID',
+    `repo`                       varchar(128)       DEFAULT NULL COMMENT 'repository 仓库地址',
+    `repo_old`                   varchar(128)       DEFAULT NULL COMMENT 'repository 旧仓库地址',
+    `create_time`                varchar(50)        DEFAULT NULL COMMENT '创建时间',
+    `update_time`                varchar(50)        DEFAULT NULL COMMENT '修改时间',
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
