ee9a74f6d0b9b800d96fa3520262be39f1dc49dc
harris <ma402847591@gmail.com>
2023-01-19 23:57:14 +0800

    feat(echarts): 升级yarn,升级echarts

--- frontend/src/common/js/chart.js
+++ frontend/src/common/js/chart.js
@@ -1,5 +1,4 @@
-import ECharts from "vue-echarts";
-import 'echarts/lib/echarts';
+import VueECharts from "vue-echarts";
 import "echarts/lib/chart/line";
 import "echarts/lib/chart/bar";
 import "echarts/lib/chart/pie";
@@ -12,6 +11,6 @@ import "zrender/lib/svg/svg";
 
 export default {
   install(Vue) {
-    Vue.component("chart", ECharts);
+    Vue.component("chart", VueECharts);
   }
 }
