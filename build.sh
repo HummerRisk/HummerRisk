65e24e31fca73f749f24183abe58bd32f677e520
harris <ma402847591@gmail.com>
2023-01-24 23:37:26 +0800

    refactor(build): 去除旧build文件

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "构建镜像 ..."
-mvn clean package -X -U -Dmaven.test.skip=true
-
-docker build -t registry.cn-beijing.aliyuncs.com/hummerrisk/hummerrisk:v0.1 --platform linux/x86_64 .
-docker push registry.cn-beijing.aliyuncs.com/hummerrisk/hummerrisk:v0.1
-docker images|grep hummerrisk|awk '{print "docker rmi -f "$3}'|sh
