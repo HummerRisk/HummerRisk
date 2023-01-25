0b40fae2554cd903eed6ec6fc80fa2c7161ffaa6
harris <ma402847591@gmail.com>
2023-01-26 03:19:23 +0800

    refactor(cloud): hummer-cloud

---
+++ hummer-modules/hummer-cloud/src/main/java/com/hummer/system/CloudApplication.java
@@ -0,0 +1,31 @@
+package com.hummer.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.hummer.common.security.annotation.EnableCustomConfig;
+import com.hummer.common.security.annotation.EnableRyFeignClients;
+import com.hummer.common.swagger.annotation.EnableCustomSwagger2;
+
+/**
+ * 混合云模块
+ *
+ * @author harris1943
+ */
+@EnableCustomConfig
+@EnableCustomSwagger2
+@EnableRyFeignClients
+@SpringBootApplication
+public class CloudApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CloudApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  混合云模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                "  ██╗  ██╗██╗   ██╗███╗   ███╗███╗   ███╗███████╗██████╗ ██████╗ ██╗███████╗██╗  ██╗\n" +
+                "  ██║  ██║██║   ██║████╗ ████║████╗ ████║██╔════╝██╔══██╗██╔══██╗██║██╔════╝██║ ██╔╝\n" +
+                "  ███████║██║   ██║██╔████╔██║██╔████╔██║█████╗  ██████╔╝██████╔╝██║███████╗█████╔╝\n" +
+                "  ██╔══██║██║   ██║██║╚██╔╝██║██║╚██╔╝██║██╔══╝  ██╔══██╗██╔══██╗██║╚════██║██╔═██╗\n" +
+                "  ██║  ██║╚██████╔╝██║ ╚═╝ ██║██║ ╚═╝ ██║███████╗██║  ██║██║  ██║██║███████║██║  ██╗\n" +
+                "  ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝");
+    }
+}
