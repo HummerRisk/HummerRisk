50f848306321d82cdc8e4b644682d8c1785975dc
harris <ma402847591@gmail.com>
2023-01-25 23:58:08 +0800

    refactor(job): hummer-job

---
+++ hummer-modules/hummer-job/src/main/java/com/hummer/job/JobApplication.java
@@ -0,0 +1,31 @@
+package com.hummer.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.hummer.common.security.annotation.EnableCustomConfig;
+import com.hummer.common.security.annotation.EnableRyFeignClients;
+import com.hummer.common.swagger.annotation.EnableCustomSwagger2;
+
+/**
+ * 定时任务
+ *
+ * @author harris1943
+ */
+@EnableCustomConfig
+@EnableCustomSwagger2
+@EnableRyFeignClients
+@SpringBootApplication
+public class JobApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JobApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  定时任务模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                "  ██╗  ██╗██╗   ██╗███╗   ███╗███╗   ███╗███████╗██████╗ ██████╗ ██╗███████╗██╗  ██╗\n" +
+                "  ██║  ██║██║   ██║████╗ ████║████╗ ████║██╔════╝██╔══██╗██╔══██╗██║██╔════╝██║ ██╔╝\n" +
+                "  ███████║██║   ██║██╔████╔██║██╔████╔██║█████╗  ██████╔╝██████╔╝██║███████╗█████╔╝\n" +
+                "  ██╔══██║██║   ██║██║╚██╔╝██║██║╚██╔╝██║██╔══╝  ██╔══██╗██╔══██╗██║╚════██║██╔═██╗\n" +
+                "  ██║  ██║╚██████╔╝██║ ╚═╝ ██║██║ ╚═╝ ██║███████╗██║  ██║██║  ██║██║███████║██║  ██╗\n" +
+                "  ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝");
+    }
+}
