1cd35af1910a38dd336122ea582ca0cbaf03ab42
harris <ma402847591@gmail.com>
2023-01-26 03:19:45 +0800

    refactor(k8s): hummer-k8s

---
+++ hummer-modules/hummer-k8s/src/main/java/com/hummer/system/K8sApplication.java
@@ -0,0 +1,31 @@
+package com.hummer.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.hummer.common.security.annotation.EnableCustomConfig;
+import com.hummer.common.security.annotation.EnableRyFeignClients;
+import com.hummer.common.swagger.annotation.EnableCustomSwagger2;
+
+/**
+ * 云原生模块
+ *
+ * @author harris1943
+ */
+@EnableCustomConfig
+@EnableCustomSwagger2
+@EnableRyFeignClients
+@SpringBootApplication
+public class K8sApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(K8sApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  云原生模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                "  ██╗  ██╗██╗   ██╗███╗   ███╗███╗   ███╗███████╗██████╗ ██████╗ ██╗███████╗██╗  ██╗\n" +
+                "  ██║  ██║██║   ██║████╗ ████║████╗ ████║██╔════╝██╔══██╗██╔══██╗██║██╔════╝██║ ██╔╝\n" +
+                "  ███████║██║   ██║██╔████╔██║██╔████╔██║█████╗  ██████╔╝██████╔╝██║███████╗█████╔╝\n" +
+                "  ██╔══██║██║   ██║██║╚██╔╝██║██║╚██╔╝██║██╔══╝  ██╔══██╗██╔══██╗██║╚════██║██╔═██╗\n" +
+                "  ██║  ██║╚██████╔╝██║ ╚═╝ ██║██║ ╚═╝ ██║███████╗██║  ██║██║  ██║██║███████║██║  ██╗\n" +
+                "  ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝");
+    }
+}
