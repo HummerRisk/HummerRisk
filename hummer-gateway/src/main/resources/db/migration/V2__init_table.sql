34110a59a7532d9d2ce5cb71c5b64dcec1aaaf54
harris <ma402847591@gmail.com>
2023-01-30 23:15:46 +0800

    refactor(gateway): nacos sql

--- hummer-gateway/src/main/resources/db/migration/V2__init_table.sql
+++ hummer-gateway/src/main/resources/db/migration/V2__init_table.sql
@@ -122,25 +122,25 @@ CREATE TABLE `group_capacity`
 /******************************************/
 /*   表名称 = his_config_info   */
 /******************************************/
-CREATE TABLE `his_config_info`
-(
-    `id`           bigint(64) unsigned NOT NULL,
-    `nid`          bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `data_id`      varchar(255) NOT NULL,
-    `group_id`     varchar(128) NOT NULL,
-    `app_name`     varchar(128)          DEFAULT NULL COMMENT 'app_name',
-    `content`      longtext     NOT NULL,
-    `md5`          varchar(32)           DEFAULT NULL,
-    `gmt_create`   datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `gmt_modified` datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `src_user`     text,
-    `src_ip`       varchar(50)           DEFAULT NULL,
-    `op_type`      char(10)              DEFAULT NULL,
-    `tenant_id`    varchar(128)          DEFAULT '' COMMENT '租户字段',
-    PRIMARY KEY (`nid`),
-    KEY            `idx_gmt_create` (`gmt_create`),
-    KEY            `idx_gmt_modified` (`gmt_modified`),
-    KEY            `idx_did` (`data_id`)
+CREATE TABLE `his_config_info` (
+   `id` bigint(64) unsigned NOT NULL,
+   `nid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+   `data_id` varchar(255) NOT NULL,
+   `group_id` varchar(128) NOT NULL,
+   `app_name` varchar(128) DEFAULT NULL COMMENT 'app_name',
+   `content` longtext NOT NULL,
+   `md5` varchar(32) DEFAULT NULL,
+   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   `src_user` text,
+   `src_ip` varchar(50) DEFAULT NULL,
+   `op_type` char(10) DEFAULT NULL,
+   `tenant_id` varchar(128) DEFAULT '' COMMENT '租户字段',
+   `encrypted_data_key` text COMMENT '秘钥',
+   PRIMARY KEY (`nid`),
+   KEY `idx_gmt_create` (`gmt_create`),
+   KEY `idx_gmt_modified` (`gmt_modified`),
+   KEY `idx_did` (`data_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='多租户改造';
 
 
@@ -148,34 +148,68 @@ CREATE TABLE `his_config_info`
 /*   数据库全名 = nacos_config   */
 /*   表名称 = tenant_capacity   */
 /******************************************/
-CREATE TABLE `tenant_capacity`
-(
-    `id`                bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-    `tenant_id`         varchar(128) NOT NULL DEFAULT '' COMMENT 'Tenant ID',
-    `quota`             int(10) unsigned NOT NULL DEFAULT '0' COMMENT '配额，0表示使用默认值',
-    `usage`             int(10) unsigned NOT NULL DEFAULT '0' COMMENT '使用量',
-    `max_size`          int(10) unsigned NOT NULL DEFAULT '0' COMMENT '单个配置大小上限，单位为字节，0表示使用默认值',
-    `max_aggr_count`    int(10) unsigned NOT NULL DEFAULT '0' COMMENT '聚合子配置最大个数',
-    `max_aggr_size`     int(10) unsigned NOT NULL DEFAULT '0' COMMENT '单个聚合数据的子配置大小上限，单位为字节，0表示使用默认值',
-    `max_history_count` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '最大变更历史数量',
-    `gmt_create`        datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `gmt_modified`      datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `uk_tenant_id` (`tenant_id`)
+CREATE TABLE `tenant_capacity` (
+                                   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                                   `tenant_id` varchar(128) NOT NULL DEFAULT '' COMMENT 'Tenant ID',
+                                   `quota` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '配额，0表示使用默认值',
+                                   `usage` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '使用量',
+                                   `max_size` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '单个配置大小上限，单位为字节，0表示使用默认值',
+                                   `max_aggr_count` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '聚合子配置最大个数',
+                                   `max_aggr_size` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '单个聚合数据的子配置大小上限，单位为字节，0表示使用默认值',
+                                   `max_history_count` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '最大变更历史数量',
+                                   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+                                   PRIMARY KEY (`id`),
+                                   UNIQUE KEY `uk_tenant_id` (`tenant_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='租户容量信息表';
 
 
-CREATE TABLE `tenant_info`
-(
-    `id`            bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
-    `kp`            varchar(128) NOT NULL COMMENT 'kp',
-    `tenant_id`     varchar(128) default '' COMMENT 'tenant_id',
-    `tenant_name`   varchar(128) default '' COMMENT 'tenant_name',
-    `tenant_desc`   varchar(256) DEFAULT NULL COMMENT 'tenant_desc',
-    `create_source` varchar(32)  DEFAULT NULL COMMENT 'create_source',
-    `gmt_create`    bigint(20) NOT NULL COMMENT '创建时间',
-    `gmt_modified`  bigint(20) NOT NULL COMMENT '修改时间',
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `uk_tenant_info_kptenantid` (`kp`,`tenant_id`),
-    KEY             `idx_tenant_id` (`tenant_id`)
+CREATE TABLE `tenant_info` (
+                               `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+                               `kp` varchar(128) NOT NULL COMMENT 'kp',
+                               `tenant_id` varchar(128) default '' COMMENT 'tenant_id',
+                               `tenant_name` varchar(128) default '' COMMENT 'tenant_name',
+                               `tenant_desc` varchar(256) DEFAULT NULL COMMENT 'tenant_desc',
+                               `create_source` varchar(32) DEFAULT NULL COMMENT 'create_source',
+                               `gmt_create` bigint(20) NOT NULL COMMENT '创建时间',
+                               `gmt_modified` bigint(20) NOT NULL COMMENT '修改时间',
+                               PRIMARY KEY (`id`),
+                               UNIQUE KEY `uk_tenant_info_kptenantid` (`kp`,`tenant_id`),
+                               KEY `idx_tenant_id` (`tenant_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='tenant_info';
+
+CREATE TABLE `users` (
+                         `username` varchar(50) NOT NULL PRIMARY KEY,
+                         `password` varchar(500) NOT NULL,
+                         `enabled` boolean NOT NULL
+);
+
+CREATE TABLE `roles` (
+                         `username` varchar(50) NOT NULL,
+                         `role` varchar(50) NOT NULL,
+                         UNIQUE INDEX `idx_user_role` (`username` ASC, `role` ASC) USING BTREE
+);
+
+CREATE TABLE `permissions` (
+                               `role` varchar(50) NOT NULL,
+                               `resource` varchar(255) NOT NULL,
+                               `action` varchar(8) NOT NULL,
+                               UNIQUE INDEX `uk_role_permission` (`role`,`resource`,`action`) USING BTREE
+);
+
+INSERT INTO `users` (username, password, enabled) VALUES ('nacos', '$2a$10$EuWPZHzz32dJN7jexM34MOeYirDdFAZm2kuWj7VEOJhhZkDrxfvUu', TRUE);
+
+INSERT INTO `roles` (username, role) VALUES ('nacos', 'ROLE_ADMIN');
+
+INSERT INTO config_info(id, data_id, group_id, content, md5, gmt_create, gmt_modified, src_user, src_ip, app_name, tenant_id, c_desc, c_use, effect, type, c_schema, encrypted_data_key) values
+(1,'application-dev.yml','DEFAULT_GROUP','spring:\n  autoconfigure:\n    exclude: com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure\n  mvc:\n    pathmatch:\n      matching-strategy: ant_path_matcher\n\n# feign 配置\nfeign:\n  sentinel:\n    enabled: true\n  okhttp:\n    enabled: true\n  httpclient:\n    enabled: false\n  client:\n    config:\n      default:\n        connectTimeout: 10000\n        readTimeout: 10000\n  compression:\n    request:\n      enabled: true\n    response:\n      enabled: true\n\n# 暴露监控端点\nmanagement:\n  endpoints:\n    web:\n      exposure:\n        include: \'*\'\n','aaa73b809cfd4d0058893aa13da57806','2020-05-20 12:00:00','2022-04-24 10:26:34','nacos','0:0:0:0:0:0:0:1','','','通用配置','null','null','yaml',NULL,''),
+(2,'hummer-gateway-dev.yml','DEFAULT_GROUP','spring:\n  redis:\n    host: localhost\n    port: 6379\n    password:\n  cloud:\n    gateway:\n      discovery:\n        locator:\n          lowerCaseServiceId: true\n          enabled: true\n      routes:\n        # 认证中心\n        - id: hummer-auth\n          uri: lb://hummer-auth\n          predicates:\n            - Path=/auth/**\n          filters:\n            # 验证码处理\n            - CacheRequestFilter\n            - ValidateCodeFilter\n            - StripPrefix=1\n        # 代码生成\n        - id: hummer-gen\n          uri: lb://hummer-gen\n          predicates:\n            - Path=/code/**\n          filters:\n            - StripPrefix=1\n        # 定时任务\n        - id: hummer-job\n          uri: lb://hummer-job\n          predicates:\n            - Path=/schedule/**\n          filters:\n            - StripPrefix=1\n        # 系统模块\n        - id: hummer-system\n          uri: lb://hummer-system\n          predicates:\n            - Path=/system/**\n          filters:\n            - StripPrefix=1\n        # 文件服务\n        - id: hummer-file\n          uri: lb://hummer-file\n          predicates:\n            - Path=/file/**\n          filters:\n            - StripPrefix=1\n\n# 安全配置\nsecurity:\n  # 验证码\n  captcha:\n    enabled: true\n    type: math\n  # 防止XSS攻击\n  xss:\n    enabled: true\n    excludeUrls:\n      - /system/notice\n  # 不校验白名单\n  ignore:\n    whites:\n      - /auth/logout\n      - /auth/login\n      - /auth/register\n      - /*/v2/api-docs\n      - /csrf\n','57cec5abd0e0a6b77d853750344a9dc0','2020-05-14 14:17:55','2022-09-29 02:48:32','nacos','0:0:0:0:0:0:0:1','','','网关模块','null','null','yaml','',''),
+(3,'hummer-auth-dev.yml','DEFAULT_GROUP','spring:\n  redis:\n    host: localhost\n    port: 6379\n    password:\n','8bd9dada9a94822feeab40de55efced6','2020-11-20 00:00:00','2022-09-29 02:48:42','nacos','0:0:0:0:0:0:0:1','','','认证中心','null','null','yaml','',''),
+(4,'hummer-monitor-dev.yml','DEFAULT_GROUP','# spring\nspring:\n  security:\n    user:\n      name: hummer\n      password: 123456\n  boot:\n    admin:\n      ui:\n        title: 若依服务状态监控\n','6f122fd2bfb8d45f858e7d6529a9cd44','2020-11-20 00:00:00','2022-09-29 02:48:54','nacos','0:0:0:0:0:0:0:1','','','监控中心','null','null','yaml','',''),
+(5,'hummer-system-dev.yml','DEFAULT_GROUP','# spring配置\nspring:\n  redis:\n    host: localhost\n    port: 6379\n    password:\n  datasource:\n    druid:\n      stat-view-servlet:\n        enabled: true\n        loginUsername: admin\n        loginPassword: 123456\n    dynamic:\n      druid:\n        initial-size: 5\n        min-idle: 5\n        maxActive: 20\n        maxWait: 60000\n        timeBetweenEvictionRunsMillis: 60000\n        minEvictableIdleTimeMillis: 300000\n        validationQuery: SELECT 1 FROM DUAL\n        testWhileIdle: true\n        testOnBorrow: false\n        testOnReturn: false\n        poolPreparedStatements: true\n        maxPoolPreparedStatementPerConnectionSize: 20\n        filters: stat,slf4j\n        connectionProperties: druid.stat.mergeSql\\=true;druid.stat.slowSqlMillis\\=5000\n      datasource:\n          # 主库数据源\n          master:\n            driver-class-name: com.mysql.cj.jdbc.Driver\n            url: jdbc:mysql://localhost:3306/ry-cloud?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8\n            username: root\n            password: password\n          # 从库数据源\n          # slave:\n            # username: \n            # password: \n            # url: \n            # driver-class-name: \n\n# mybatis配置\nmybatis:\n    # 搜索指定包别名\n    typeAliasesPackage: com.hummer.system\n    # 配置mapper的扫描，找到所有的mapper.xml映射文件\n    mapperLocations: classpath:mapper/**/*.xml\n\n# swagger配置\nswagger:\n  title: 系统模块接口文档\n  license: Powered By hummer\n  licenseUrl: https://hummer.vip','48e0ed4a040c402bdc2444213a82c910','2020-11-20 00:00:00','2022-09-29 02:49:09','nacos','0:0:0:0:0:0:0:1','','','系统模块','null','null','yaml','',''),
+(6,'hummer-gen-dev.yml','DEFAULT_GROUP','# spring配置\nspring:\n  redis:\n    host: localhost\n    port: 6379\n    password:\n  datasource:\n    driver-class-name: com.mysql.cj.jdbc.Driver\n    url: jdbc:mysql://localhost:3306/ry-cloud?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8\n    username: root\n    password: password\n\n# mybatis配置\nmybatis:\n    # 搜索指定包别名\n    typeAliasesPackage: com.hummer.gen.domain\n    # 配置mapper的扫描，找到所有的mapper.xml映射文件\n    mapperLocations: classpath:mapper/**/*.xml\n\n# swagger配置\nswagger:\n  title: 代码生成接口文档\n  license: Powered By hummer\n  licenseUrl: https://hummer.vip\n\n# 代码生成\ngen:\n  # 作者\n  author: hummer\n  # 默认生成包路径 system 需改成自己的模块名称 如 system monitor tool\n  packageName: com.hummer.system\n  # 自动去除表前缀，默认是false\n  autoRemovePre: false\n  # 表前缀（生成类名不会包含表前缀，多个用逗号分隔）\n  tablePrefix: sys_\n','eb592420b3fceae1402881887b8a6a0d','2020-11-20 00:00:00','2022-09-29 02:49:42','nacos','0:0:0:0:0:0:0:1','','','代码生成','null','null','yaml','',''),
+(7,'hummer-job-dev.yml','DEFAULT_GROUP','# spring配置\nspring:\n  redis:\n    host: localhost\n    port: 6379\n    password: \n  datasource:\n    driver-class-name: com.mysql.cj.jdbc.Driver\n    url: jdbc:mysql://localhost:3306/ry-cloud?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8\n    username: root\n    password: password\n\n# mybatis配置\nmybatis:\n    # 搜索指定包别名\n    typeAliasesPackage: com.hummer.job.domain\n    # 配置mapper的扫描，找到所有的mapper.xml映射文件\n    mapperLocations: classpath:mapper/**/*.xml\n\n# swagger配置\nswagger:\n  title: 定时任务接口文档\n  license: Powered By hummer\n  licenseUrl: https://hummer.vip\n','edcf0e3fe13fea07b4ec08b1088f30b3','2020-11-20 00:00:00','2022-09-29 02:50:50','nacos','0:0:0:0:0:0:0:1','','','定时任务','null','null','yaml','',''),
+(8,'hummer-file-dev.yml','DEFAULT_GROUP','# 本地文件上传    \r\nfile:\r\n    domain: http://127.0.0.1:9300\r\n    path: D:/hummer/uploadPath\r\n    prefix: /statics\r\n\r\n# FastDFS配置\r\nfdfs:\r\n  domain: http://8.129.231.12\r\n  soTimeout: 3000\r\n  connectTimeout: 2000\r\n  trackerList: 8.129.231.12:22122\r\n\r\n# Minio配置\r\nminio:\r\n  url: http://8.129.231.12:9000\r\n  accessKey: minioadmin\r\n  secretKey: minioadmin\r\n  bucketName: test','5382b93f3d8059d6068c0501fdd41195','2020-11-20 00:00:00','2020-12-21 21:01:59',NULL,'0:0:0:0:0:0:0:1','','','文件服务','null','null','yaml',NULL,''),
+(9,'hummer-file-dev.yml','DEFAULT_GROUP','# 本地文件上传    \r\nfile:\r\n    domain: http://127.0.0.1:9300\r\n    path: D:/hummer/uploadPath\r\n    prefix: /statics\r\n\r\n# FastDFS配置\r\nfdfs:\r\n  domain: http://8.129.231.12\r\n  soTimeout: 3000\r\n  connectTimeout: 2000\r\n  trackerList: 8.129.231.12:22122\r\n\r\n# Minio配置\r\nminio:\r\n  url: http://8.129.231.12:9000\r\n  accessKey: minioadmin\r\n  secretKey: minioadmin\r\n  bucketName: test','5382b93f3d8059d6068c0501fdd41195','2020-11-20 00:00:00','2020-12-21 21:01:59',NULL,'0:0:0:0:0:0:0:1','','','文件服务','null','null','yaml',NULL,''),
+(10,'hummer-file-dev.yml','DEFAULT_GROUP','# 本地文件上传    \r\nfile:\r\n    domain: http://127.0.0.1:9300\r\n    path: D:/hummer/uploadPath\r\n    prefix: /statics\r\n\r\n# FastDFS配置\r\nfdfs:\r\n  domain: http://8.129.231.12\r\n  soTimeout: 3000\r\n  connectTimeout: 2000\r\n  trackerList: 8.129.231.12:22122\r\n\r\n# Minio配置\r\nminio:\r\n  url: http://8.129.231.12:9000\r\n  accessKey: minioadmin\r\n  secretKey: minioadmin\r\n  bucketName: test','5382b93f3d8059d6068c0501fdd41195','2020-11-20 00:00:00','2020-12-21 21:01:59',NULL,'0:0:0:0:0:0:0:1','','','文件服务','null','null','yaml',NULL,''),
+(11,'sentinel-hummer-gateway','DEFAULT_GROUP','[\r\n    {\r\n        \"resource\": \"hummer-auth\",\r\n        \"count\": 500,\r\n        \"grade\": 1,\r\n        \"limitApp\": \"default\",\r\n        \"strategy\": 0,\r\n        \"controlBehavior\": 0\r\n    },\r\n	{\r\n        \"resource\": \"hummer-system\",\r\n        \"count\": 1000,\r\n        \"grade\": 1,\r\n        \"limitApp\": \"default\",\r\n        \"strategy\": 0,\r\n        \"controlBehavior\": 0\r\n    },\r\n	{\r\n        \"resource\": \"hummer-gen\",\r\n        \"count\": 200,\r\n        \"grade\": 1,\r\n        \"limitApp\": \"default\",\r\n        \"strategy\": 0,\r\n        \"controlBehavior\": 0\r\n    },\r\n	{\r\n        \"resource\": \"hummer-job\",\r\n        \"count\": 300,\r\n        \"grade\": 1,\r\n        \"limitApp\": \"default\",\r\n        \"strategy\": 0,\r\n        \"controlBehavior\": 0\r\n    }\r\n]','9f3a3069261598f74220bc47958ec252','2020-11-20 00:00:00','2020-11-20 00:00:00',NULL,'0:0:0:0:0:0:0:1','','','限流策略','null','null','json',NULL,'');
