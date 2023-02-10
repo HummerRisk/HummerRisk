bf88559845405a94a9e10fb6c8e6ccb511e554d1
harris <ma402847591@gmail.com>
2023-02-11 01:01:56 +0800

    refactor(datasource): datasource

--- hummer-common/hummer-common-datasource/src/main/java/com/hummer/common/datasource/annotation/Slave.java
+++ hummer-common/hummer-common-datasource/src/main/java/com/hummer/common/datasource/annotation/Slave.java
@@ -5,18 +5,18 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 import com.baomidou.dynamic.datasource.annotation.DS;
 
 /**
  * 从库数据源
- * 
+ *
  * @author harris1943
  */
-@Target({ ElementType.TYPE, ElementType.METHOD })
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @DS("slave")
-public @interface Slave
-{
+public @interface Slave {
 
 }
