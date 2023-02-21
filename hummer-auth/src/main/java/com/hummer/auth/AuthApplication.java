ac3013ed34b423e11bc5a59fc8acb0a76299b0b6
harris <ma402847591@gmail.com>
2023-02-21 22:29:50 +0800

    refactor(auth): 解决 auth 项目报错

--- hummer-auth/src/main/java/com/hummer/auth/AuthApplication.java
+++ hummer-auth/src/main/java/com/hummer/auth/AuthApplication.java
@@ -15,8 +15,8 @@ import com.hummer.common.security.annotation.EnableHummerFeignClients;
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
-        System.out.println("{HTTP/1.1, (http/1.1)}{0.0.0.0:9200}");
-        System.out.println("(♥◠‿◠)ﾉﾞ  认证授权中心启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+        System.out.println("    {HTTP/1.1, (http/1.1)}{0.0.0.0:9200}");
+        System.out.println("    (♥◠‿◠)ﾉﾞ  认证授权中心启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
                 "  ██╗  ██╗██╗   ██╗███╗   ███╗███╗   ███╗███████╗██████╗ ██████╗ ██╗███████╗██╗  ██╗\n" +
                 "  ██║  ██║██║   ██║████╗ ████║████╗ ████║██╔════╝██╔══██╗██╔══██╗██║██╔════╝██║ ██╔╝\n" +
                 "  ███████║██║   ██║██╔████╔██║██╔████╔██║█████╗  ██████╔╝██████╔╝██║███████╗█████╔╝\n" +
