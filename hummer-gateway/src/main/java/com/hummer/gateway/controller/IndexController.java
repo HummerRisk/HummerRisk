3d0698a689c8b128a27f66845735ee9e188293f3
harris <ma402847591@gmail.com>
2023-02-26 22:13:35 +0800

    refactor(gateway): 添加 healthz,返回字符串而非页面

---
+++ hummer-gateway/src/main/java/com/hummer/gateway/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.hummer.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class IndexController {
+
+    @GetMapping(value = "healthz")
+    public String healthz() {
+        return "SUCCESS";
+    }
+
+}
