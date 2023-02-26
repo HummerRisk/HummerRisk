cd448ef3cfadbb5f906fd8a7727bcddfa0d8fb62
wangjiankun233 <wjk5442908@163.com>
2023-02-26 21:43:08 +0800

    fix(flyway):解决 flyway 不执行问题

--- hummer-auth/src/main/resources/db.migration/V39__alter_web_msg.sql
+++
@@ -1,8 +0,0 @@
-
-
-ALTER TABLE `web_msg` ADD `result_id` longtext DEFAULT NULL COMMENT '检测结果ID';
-
-UPDATE `rule` SET `script` = 'policies:\n    - name: qiniu-bucket-private\n      resource: qiniu.kodo\n      filters:\n        - type: private\n          value: ${{value}}' WHERE id = '1268870b-188d-41a8-a86e-0c83299dc93c';
-
-UPDATE `rule_type` SET `resource_type` = 'qiniu.kodo' WHERE id = 'c0cd80bf-259f-4a46-a1a8-9ce7edc5e592';
-
