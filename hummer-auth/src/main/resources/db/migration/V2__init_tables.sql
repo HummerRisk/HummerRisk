56a13beaac6317c2d5818b74a17e86cae01872d1
harris <ma402847591@gmail.com>
2023-02-27 23:08:19 +0800

    refactor(sql): mysql 8.0 写法，不用int(M)显示宽度

--- hummer-auth/src/main/resources/db/migration/V2__init_tables.sql
+++ hummer-auth/src/main/resources/db/migration/V2__init_tables.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS role (
     `name`        varchar(64)       NOT NULL COMMENT 'Role name',
     `description` varchar(255)      DEFAULT NULL COMMENT 'Role description',
     `type`        varchar(50)       DEFAULT NULL COMMENT 'Role type, (system)',
-    `create_time` bigint(13)        NOT NULL COMMENT 'Create timestamp',
-    `update_time` bigint(13)        NOT NULL COMMENT 'Update timestamp',
+    `create_time` bigint            NOT NULL COMMENT 'Create timestamp',
+    `update_time` bigint            NOT NULL COMMENT 'Update timestamp',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS system_parameter (
     `param_key`   varchar(64) CHARACTER SET utf8mb4 NOT NULL COMMENT 'Parameter name',
     `param_value` varchar(2046)                     DEFAULT NULL COMMENT 'Parameter value',
     `type`        varchar(100)                      NOT NULL DEFAULT 'text' COMMENT 'Parameter type',
-    `sort`        int(5)                            DEFAULT NULL COMMENT 'Sort',
+    `sort`        int                               DEFAULT NULL COMMENT 'Sort',
     PRIMARY KEY (`param_key`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
@@ -22,8 +22,8 @@ CREATE TABLE IF NOT EXISTS user (
     `email`                varchar(64)      NOT NULL COMMENT 'E-Mail address',
     `password`             varchar(256)     COLLATE utf8mb4_bin DEFAULT NULL,
     `status`               varchar(50)      NOT NULL COMMENT 'User status',
-    `create_time`          bigint(13)       NOT NULL COMMENT 'Create timestamp',
-    `update_time`          bigint(13)       NOT NULL COMMENT 'Update timestamp',
+    `create_time`          bigint           NOT NULL COMMENT 'Create timestamp',
+    `update_time`          bigint           NOT NULL COMMENT 'Update timestamp',
     `language`             varchar(30)      DEFAULT NULL,
     `last_account_id`      varchar(50)      DEFAULT NULL,
     `phone`                varchar(50)      DEFAULT NULL COMMENT 'Phone number of user',
@@ -37,8 +37,8 @@ CREATE TABLE IF NOT EXISTS user_role (
     `user_id`     varchar(50)       NOT NULL COMMENT 'User ID of this user-role info',
     `role_id`     varchar(50)       NOT NULL COMMENT 'Role ID of this user-role info',
     `source_id`   varchar(50)       DEFAULT NULL COMMENT 'The (system) ID of this user-role info',
-    `create_time` bigint(13)        NOT NULL COMMENT 'Create timestamp',
-    `update_time` bigint(13)        NOT NULL COMMENT 'Update timestamp',
+    `create_time` bigint            NOT NULL COMMENT 'Create timestamp',
+    `update_time` bigint            NOT NULL COMMENT 'Update timestamp',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
