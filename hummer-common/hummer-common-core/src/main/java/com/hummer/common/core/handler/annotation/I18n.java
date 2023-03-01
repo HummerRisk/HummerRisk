e2a764964aa4667b07be2f083f2bd10e7ee26c1a
harris <ma402847591@gmail.com>
2023-03-01 22:23:00 +0800

    refactor(core): 修改 core

---
+++ hummer-common/hummer-common-core/src/main/java/com/hummer/common/core/handler/annotation/I18n.java
@@ -0,0 +1,14 @@
+package com.hummer.common.core.handler.annotation;
+
+
+import com.hummer.common.core.constant.I18nConstants;
+
+import java.lang.annotation.*;
+
+@Documented
+@Inherited
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface I18n {
+    String value() default I18nConstants.LANG_COOKIE_NAME;
+}
