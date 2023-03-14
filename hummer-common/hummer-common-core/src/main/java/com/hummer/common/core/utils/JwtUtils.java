0fb182c412ecb52a5d288c1114bdec83b9908bd6
harris <ma402847591@gmail.com>
2023-03-15 05:57:29 +0800

    refactor(user): sigout

--- hummer-common/hummer-common-core/src/main/java/com/hummer/common/core/utils/JwtUtils.java
+++ hummer-common/hummer-common-core/src/main/java/com/hummer/common/core/utils/JwtUtils.java
@@ -1,6 +1,7 @@
 package com.hummer.common.core.utils;
 
 import java.util.Map;
+
 import com.hummer.common.core.constant.SecurityConstants;
 import com.hummer.common.core.constant.TokenConstants;
 import com.hummer.common.core.text.Convert;
@@ -13,8 +14,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
  *
  * @author harris1943
  */
-public class JwtUtils
-{
+public class JwtUtils {
     public static String secret = TokenConstants.SECRET;
 
     /**
@@ -23,8 +23,7 @@ public class JwtUtils
      * @param claims 数据声明
      * @return 令牌
      */
-    public static String createToken(Map<String, Object> claims)
-    {
+    public static String createToken(Map<String, Object> claims) {
         String token = Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS512, secret).compact();
         return token;
     }
@@ -35,89 +34,81 @@ public class JwtUtils
      * @param token 令牌
      * @return 数据声明
      */
-    public static Claims parseToken(String token)
-    {
+    public static Claims parseToken(String token) {
         return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
     }
 
     /**
      * 根据令牌获取用户标识
-     * 
+     *
      * @param token 令牌
      * @return 用户ID
      */
-    public static String getUserKey(String token)
-    {
+    public static String getUserKey(String token) {
         Claims claims = parseToken(token);
         return getValue(claims, SecurityConstants.USER_KEY);
     }
 
     /**
      * 根据令牌获取用户标识
-     * 
+     *
      * @param claims 身份信息
      * @return 用户ID
      */
-    public static String getUserKey(Claims claims)
-    {
+    public static String getUserKey(Claims claims) {
         return getValue(claims, SecurityConstants.USER_KEY);
     }
 
     /**
      * 根据令牌获取用户ID
-     * 
+     *
      * @param token 令牌
      * @return 用户ID
      */
-    public static String getUserId(String token)
-    {
+    public static String getUserId(String token) {
         Claims claims = parseToken(token);
         return getValue(claims, SecurityConstants.DETAILS_USER_ID);
     }
 
     /**
      * 根据身份信息获取用户ID
-     * 
+     *
      * @param claims 身份信息
      * @return 用户ID
      */
-    public static String getUserId(Claims claims)
-    {
+    public static String getUserId(Claims claims) {
         return getValue(claims, SecurityConstants.DETAILS_USER_ID);
     }
 
     /**
      * 根据令牌获取用户名
-     * 
+     *
      * @param token 令牌
      * @return 用户名
      */
-    public static String getUserName(String token)
-    {
+    public static String getUserName(String token) {
         Claims claims = parseToken(token);
         return getValue(claims, SecurityConstants.DETAILS_USERNAME);
     }
 
     /**
      * 根据身份信息获取用户名
-     * 
+     *
      * @param claims 身份信息
      * @return 用户名
      */
-    public static String getUserName(Claims claims)
-    {
+    public static String getUserName(Claims claims) {
         return getValue(claims, SecurityConstants.DETAILS_USERNAME);
     }
 
     /**
      * 根据身份信息获取键值
-     * 
+     *
      * @param claims 身份信息
-     * @param key 键
+     * @param key    键
      * @return 值
      */
-    public static String getValue(Claims claims, String key)
-    {
+    public static String getValue(Claims claims, String key) {
         return Convert.toStr(claims.get(key), "");
     }
 }
