6abb27f6cb216a657d0a5535bdd124fa6c256ae5
guohao <402847591@qq.com>
2023-03-14 11:31:13 +0800

    refactor(repo): repo password

--- backend/src/main/resources/db/migration/V44__alter_rule.sql
+++ backend/src/main/resources/db/migration/V44__alter_rule.sql
@@ -1,4 +1,6 @@
 
+ALTER TABLE `image_repo` MODIFY column `password` mediumtext DEFAULT NULL COMMENT '仓库密码';
+
 ALTER TABLE `rule_group` ADD `type` varchar(64) DEFAULT 'cloud' COMMENT '规则组类别';
 
 INSERT INTO `plugin` ( `id`, `name`, `icon`, `update_time`, `scan_type`, `order_`, `type`) values ('hummer-server-plugin', '主机检测', 'server.png', concat(unix_timestamp(now()), '001'), 'server', 23, 'server');
