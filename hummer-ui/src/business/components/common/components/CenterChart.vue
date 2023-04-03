81364a266926a69963fb6f6dfec2c6b7b77d3ef9
harris <ma402847591@gmail.com>
2023-04-04 03:12:13 +0800

    refactor(cloud): score chart

--- hummer-ui/src/business/components/common/components/CenterChart.vue
+++ hummer-ui/src/business/components/common/components/CenterChart.vue
@@ -27,16 +27,72 @@ export default {
     init() {
       let data = this.initData();
       this.options = {
-        backgroundColor: '#fff',
         //第一个图表
-        series: [
+        series: [{
+          type: 'pie',
+          hoverAnimation: false, //鼠标经过的特效
+          left: 0,
+          right: 0,
+          bottom: 0,
+          top: 0,
+          radius: ['75%', '90%'],
+          center: ['50%', '60%'],
+          startAngle: 225,
+          labelLine: {
+            normal: {
+              show: false
+            }
+          },
+          label: {
+            normal: {
+              position: 'center'
+            }
+          },
+          data: [{
+            value: 100,
+            itemStyle: {
+              normal: {
+                color: '#193f67'
+              }
+            },
+            label: {
+              normal: {
+                formatter: '{c}分',
+                position: 'center',
+                show: true,
+                textStyle: {
+                  fontSize: '15',
+                  fontWeight: 'normal',
+                  color: '#34374E',
+                  fontFamily: 'aril'
+                }
+              }
+            }
+          }, {
+            value: 35,
+            itemStyle: {
+              normal: {
+                label: {
+                  show: false
+                },
+                labelLine: {
+                  show: false
+                },
+                color: "rgba(0,0,0,0)",
+                borderWidth: 0
+              },
+              emphasis: {
+                color: "rgba(0,0,0,0)",
+                borderWidth: 0
+              }
+            },
+          },
+          ]
+        },
+          //上层环形配置
           {
             type: 'pie',
-            hoverAnimation: true, //鼠标经过的特效
-            left: 0,
-            right: 0,
-            bottom: 0,
-            top: 0,
+            hoverAnimation: false, //鼠标经过的特效
             radius: ['75%', '90%'],
             center: ['50%', '60%'],
             startAngle: 225,
@@ -52,10 +108,10 @@ export default {
             },
             data: [
               {
-                value: 100,
+                value: data,
                 itemStyle: {
                   normal: {
-                    color: '#E1E8EE'
+                    color: '#6F78CC'
                   }
                 },
               }, {
@@ -77,86 +133,12 @@ export default {
                   }
                 },
               },
-
-            ]
-          },
-          //上层环形配置
-          {
-            type: 'pie',
-            hoverAnimation: false, //鼠标经过的特效
-            radius: ['75%', '90%'],
-            center: ['50%', '60%'],
-            startAngle: 225,
-            labelLine: {
-              normal: {
-                show: false
-              }
-            },
-            label: {
-              normal: {
-                position: 'center',
-                show: false
-              }
-            },
-            detail: {
-              valueAnimation: true,
-              fontSize: 14,
-              offsetCenter: [0, '70%']
-            },
-            data: [
-              {
-                value: data,
-                itemStyle: {
-                  normal: {
-                    color: '#6F78CC'
-                  }
-                },
-                label: {
-                  normal: {
-                    formatter: '{c}分',
-                    position: 'center',
-                    show: true,
-                    textStyle: {
-                      fontSize: '14',
-                      fontWeight: 'normal',
-                      color: '#34374E',
-                    }
-                  }
-                },
-                emphasis: {
-                  show: false,
-                  textStyle: {
-                    fontSize: '14',
-                    fontWeight: 'bold'
-                  }
-                }
-              },
-              {
-                value: 32,
-                itemStyle: {
-                  normal: {
-                    label: {
-                      show: false
-                    },
-                    labelLine: {
-                      show: false
-                    },
-                    color: "rgba(0,0,0,0)",
-                    borderWidth: 0
-                  },
-                  emphasis: {
-                    color: "rgba(0,0,0,0)",
-                    borderWidth: 0,
-                  }
-                },
-              },
-
             ]
           },
         ]
       };
     },
-    initData () {
+    initData() {
       if (!this.row) {
         return 100;
       } else {
