1c8e4626563bdf9a2697e9e2a4d96339e1c43c91
harris <ma402847591@gmail.com>
2023-05-11 20:47:56 +0800

    refactor(server): 添加lynis 表

--- hummer-auth/src/main/resources/db/migration/V47__init_image_group.sql
+++ hummer-auth/src/main/resources/db/migration/V47__init_image_group.sql
@@ -16,3 +16,32 @@ ALTER TABLE `image` ADD `group_id` varchar(50) DEFAULT NULL COMMENT '分组Id';
 UPDATE `image` SET group_id = 'd661se75-1r8c-2s54-cbe6-351sd29e91ff';
 
 ALTER TABLE `server_result` modify COLUMN `command` mediumtext DEFAULT NULL COMMENT 'command';
+
+CREATE TABLE IF NOT EXISTS `server_lynis_result`
+(
+    `id`                         varchar(50)         NOT NULL COMMENT 'ID',
+    `server_id`                  varchar(50)         DEFAULT NULL COMMENT 'serverID',
+    `server_name`                varchar(256)        DEFAULT NULL COMMENT '虚拟机名称',
+    `result_status`              varchar(45)         DEFAULT NULL COMMENT '检测状态',
+    `create_time`                bigint              DEFAULT NULL COMMENT '创建时间',
+    `update_time`                bigint              DEFAULT NULL COMMENT '更新时间',
+    `apply_user`                 varchar(50)         DEFAULT NULL COMMENT '申请人',
+    `plugin_icon`                varchar(256)        DEFAULT 'server.png' COMMENT '图标',
+    `return_log`                 longtext            DEFAULT NULL COMMENT 'return log',
+    `lynis_log`                  longtext            DEFAULT NULL COMMENT 'lynis log',
+    `hardening_index`            bigint              DEFAULT NULL COMMENT 'Hardening index',
+    `tests_performed`            bigint              DEFAULT NULL COMMENT 'Tests performed',
+    `plugins_enabled`            bigint              DEFAULT NULL COMMENT 'Plugins enabled',
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `server_lynis_result_detail` (
+    `id`                           int                 NOT NULL AUTO_INCREMENT,
+    `lynis_id`                     varchar(50)         DEFAULT NULL COMMENT 'lynis 检测结果ID',
+    `create_time`                  bigint              DEFAULT NULL COMMENT '创建时间',
+    `operator`                     varchar(100)        DEFAULT NULL COMMENT '操作人',
+    `type`                         varchar(128)        DEFAULT NULL COMMENT '所属类型',
+    `output`                       mediumtext          DEFAULT NULL COMMENT '输出',
+    `status`                       varchar(100)        DEFAULT NULL COMMENT '状态',
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4;
