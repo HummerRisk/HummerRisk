e79956ae7bf3a1faf31b7f4fc1dfae99b15d87fc
harris <ma402847591@gmail.com>
2023-06-17 19:57:13 +0800

    refactor(sql): 新建资源关系sql

--- hummer-auth/src/main/resources/db/migration/V49__alter_resource.sql
+++ hummer-auth/src/main/resources/db/migration/V49__alter_resource.sql
@@ -2,3 +2,26 @@
 ALTER TABLE `cloud_resource_item` ADD `hummer_name` varchar(256) DEFAULT NULL COMMENT '资源别名';
 
 ALTER TABLE `resource_item` ADD `hummer_name` varchar(256) DEFAULT NULL COMMENT '资源别名';
+
+CREATE TABLE IF NOT EXISTS `cloud_resource_rela` (
+    `id`                         varchar(50)         NOT NULL COMMENT 'ID',
+    `name`                       varchar(256)        DEFAULT NULL COMMENT '资源名称',
+    `resource_item_id`           varchar(128)        DEFAULT NULL COMMENT 'cloud resource item 标识',
+    `plugin_id`                  varchar(128)        DEFAULT NULL COMMENT '云类型ID',
+    `account_id`                 varchar(128)        DEFAULT NULL COMMENT '云账号ID',
+    `region_id`                  varchar(128)        DEFAULT NULL COMMENT '云账号区域ID',
+    `resource_type`              varchar(64)         DEFAULT NULL COMMENT '资源类型',
+    `hummer_id`                  varchar(128)        DEFAULT NULL COMMENT '资源ID',
+    `x_axis`                     bigint              DEFAULT NULL COMMENT '横坐标',
+    `y_axis`                     bigint              DEFAULT NULL COMMENT '纵坐标',
+    `create_time`                bigint              DEFAULT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `cloud_resource_rela_link` (
+    `id`                         int                 NOT NULL AUTO_INCREMENT,
+    `source`                     varchar(50)         DEFAULT NULL COMMENT '资源关系ID(源)',
+    `target`                     varchar(50)         DEFAULT NULL COMMENT '资源关系ID(目标)',
+    `create_time`                bigint              DEFAULT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
