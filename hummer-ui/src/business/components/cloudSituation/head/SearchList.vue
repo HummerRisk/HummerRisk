ea5505b368cf86996de88fe03d5796d5b110771f
harris <ma402847591@gmail.com>
2023-06-25 03:30:50 +0800

    refactor(search): 优化 ui cloud search

---
+++ hummer-ui/src/business/components/cloudSituation/head/SearchList.vue
@@ -0,0 +1,107 @@
+<template>
+  <div v-loading="result.loading" style="width: 300px;">
+    <el-input :placeholder="$t('resource.search_by_name')"
+              prefix-icon="el-icon-search"
+              v-model="searchString"
+              clearable
+              class="search-input"
+              size="small"/>
+    <div v-if="items.length === 0" style="text-align: center; margin: 15px 0">
+        <span style="font-size: 15px; color: #8a8b8d;">
+          {{ $t('resource.i18n_no_data') }}
+        </span>
+    </div>
+    <div v-else style="height: 150px;overflow: auto">
+      <el-menu-item :key="i.id" v-for="i in items" @click="change(i.id, i.name)">
+        <template slot="title">
+          <div class="title">
+            <img :src="require(`@/assets/img/platform/${i.pluginIcon}`)" style="width: 16px; height: 16px; vertical-align:middle" alt=""/>
+            {{ i.name }}
+          </div>
+          <i class="el-icon-check" v-if="i.id === currentAccountId"></i>
+        </template>
+      </el-menu-item>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import {allListUrl} from "@/api/cloud/account/account";
+
+/* eslint-disable */
+export default {
+  name: "SearchList",
+  created() {
+    this.init();
+  },
+  computed: {
+  },
+  data() {
+    return {
+      result: {},
+      items: [],
+      searchArray: [],
+      searchString: '',
+      currentAccountId: 'all',
+      accountName: this.$t('rule.all'),
+    }
+  },
+  watch: {
+    searchString(val) {
+      this.query(val)
+    }
+  },
+  methods: {
+    async init () {
+      await this.$get(allListUrl, response => {
+        this.items = response.data;
+        this.items.unshift({id: 'all', name: this.$t('rule.all'), pluginIcon: 'all.png'});
+        this.searchArray = response.data;
+        if (this.items.length > 0) {
+          this.change(this.items[0].id, this.items[0].name);
+        }
+      });
+    },
+    query(queryString) {
+      this.items = queryString ? this.searchArray.filter(this.createFilter(queryString)) : this.searchArray;
+    },
+    createFilter(queryString) {
+      return item => {
+        return (item.name.toLowerCase().indexOf(queryString.toLowerCase()) !== -1);
+      };
+    },
+    change(accountId, accountName) {
+        this.currentAccountId = accountId;
+        this.accountName = accountName;
+        this.$emit("selectAccount", accountId, this.accountName);
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+.search-input {
+  padding: 0;
+  margin-top: -5px;
+}
+
+.search-input >>> .el-input__inner {
+  border-radius: 0;
+}
+
+.title {
+  display: inline-block;
+  padding-left: 15px;
+  max-width: 300px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.el-icon-check {
+  color: #773888;
+  margin-left: 10px;
+}
+</style>
--- hummer-ui/src/business/components/cloudSituation/head/TopoSwitch.vue
+++ hummer-ui/src/business/components/cloudSituation/head/TopoSwitch.vue
@@ -1,26 +1,89 @@
 <template>
-  <el-menu class="header-menu" :unique-opened="true" mode="horizontal" default-active="1" router background-color="aliceblue" active-text-color="red">
-    <!-- 不激活项目路由-->
-    <el-menu-item index="1" v-show="false">Placeholder</el-menu-item>
-    <el-submenu index="2" popper-class="submenu">
-      <template v-slot:title>
-        <span class="account-name" :title="name">
-          {{ $t('account.cloud_account') }}: {{ name }}
-        </span>
-      </template>
-      <search-list @topoSwitch="topoSwitch" @selectAccount="selectAccount"/>
-    </el-submenu>
-  </el-menu>
+  <div class="head-div">
+    <el-menu class="header-menu" :unique-opened="true" mode="horizontal" default-active="1" router background-color="aliceblue" active-text-color="red">
+      <!-- 不激活项目路由-->
+      <el-menu-item index="1" v-show="false">Placeholder</el-menu-item>
+      <el-submenu index="2" popper-class="submenu">
+        <template v-slot:title>
+          <span class="account-name" :title="name">
+            {{ $t('account.cloud_account') }}: {{ name }}
+          </span>
+        </template>
+        <search-list @selectAccount="selectAccount"/>
+      </el-submenu>
+    </el-menu>
+    <el-divider></el-divider>
+    <el-row style="padding: 10px 10px 10px 20px;">
+      <el-col :span="3">
+        <el-image style="border-radius: 50%;width: 24px; height: 24px; vertical-align:middle;" :src="require(`@/assets/img/cloudtopo/n-server.svg`)">
+          <div slot="error" class="image-slot">
+            <i class="el-icon-picture-outline"></i>
+          </div>
+        </el-image>
+        <span>{{ ' — ' + $t('cloud_topo.server_resource') }}</span>
+      </el-col>
+      <el-col :span="3">
+        <el-image style="border-radius: 50%;width: 24px; height: 24px; vertical-align:middle;" :src="require(`@/assets/img/cloudtopo/n-db.svg`)">
+          <div slot="error" class="image-slot">
+            <i class="el-icon-picture-outline"></i>
+          </div>
+        </el-image>
+        <span>{{ ' — ' + $t('cloud_topo.db_resource') }}</span>
+      </el-col>
+      <el-col :span="3">
+        <el-image style="border-radius: 50%;width: 24px; height: 24px; vertical-align:middle;" :src="require(`@/assets/img/cloudtopo/n-oss.svg`)">
+          <div slot="error" class="image-slot">
+            <i class="el-icon-picture-outline"></i>
+          </div>
+        </el-image>
+        <span>{{ ' — ' + $t('cloud_topo.oss_resource') }}</span>
+      </el-col>
+      <el-col :span="3">
+        <el-image style="border-radius: 50%;width: 24px; height: 24px; vertical-align:middle;" :src="require(`@/assets/img/cloudtopo/n-other.svg`)">
+          <div slot="error" class="image-slot">
+            <i class="el-icon-picture-outline"></i>
+          </div>
+        </el-image>
+        <span>{{ ' — ' + $t('cloud_topo.other_resource') }}</span>
+      </el-col>
+      <el-col :span="1">
+        {{ '|' }}
+      </el-col>
+      <el-col :span="3">
+        <el-image style="border-radius: 50%;width: 24px; height: 24px; vertical-align:middle;" :src="require(`@/assets/img/cloudtopo/n-server.svg`)">
+          <div slot="error" class="image-slot">
+            <i class="el-icon-picture-outline"></i>
+          </div>
+        </el-image>
+        <span>{{ ' — ' + $t('resource.n_risk') }}</span>
+      </el-col>
+      <el-col :span="3">
+        <el-image style="border-radius: 50%;width: 24px; height: 24px; vertical-align:middle;" :src="require(`@/assets/img/cloudtopo/e-server.svg`)">
+          <div slot="error" class="image-slot">
+            <i class="el-icon-picture-outline"></i>
+          </div>
+        </el-image>
+        <span>{{ ' — ' + $t('resource.have_risk') }}</span>
+      </el-col>
+      <el-col :span="3">
+        <el-image style="border-radius: 50%;width: 24px; height: 24px; vertical-align:middle;" :src="require(`@/assets/img/cloudtopo/u-server.svg`)">
+          <div slot="error" class="image-slot">
+            <i class="el-icon-picture-outline"></i>
+          </div>
+        </el-image>
+        <span>{{ ' — ' + $t('resource.uncheck') }}</span>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-import SearchList from "@/business/components/common/head/SearchList";
+import SearchList from "@/business/components/cloudSituation/head/SearchList";
 
 /* eslint-disable */
 export default {
   name: "TopoSwitch",
   props: {
-    accountName: String
   },
   components: {SearchList},
   data() {
@@ -29,15 +92,8 @@ export default {
     }
   },
   watch: {
-    accountName() {
-      this.name = this.accountName;
-    }
   },
   methods: {
-    topoSwitch(accountId, accountName) {
-      this.name = accountName;
-      this.$emit("topoSwitch", accountId);
-    },
     selectAccount(accountId, accountName) {
       this.name = accountName;
       this.$emit('selectAccount', accountId, accountName);
@@ -62,5 +118,9 @@ export default {
 .el-btn-btm {
   vertical-align: middle;
   margin: 3px 0;
+}
+
+.head-div {
+  background-color: aliceblue;
 }
 </style>
