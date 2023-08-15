22c9b7c6df2f2023a815bf7f0c1e09e62a642084
harris1943 <ma402847591@gmail.com>
2023-08-15 13:27:55 +0800

    refactor(sql): 优化名称字段短的问题

--- hummer-auth/src/main/resources/db/migration/V17__init_project.sql
+++ hummer-auth/src/main/resources/db/migration/V17__init_project.sql
@@ -89,3 +89,13 @@ CREATE TABLE IF NOT EXISTS `cloud_process_log` (
     PRIMARY KEY (`id`)
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
 
+Alter table `cloud_resource_rela` modify COLUMN `hummer_id` varchar(1024) DEFAULT NULL COMMENT '资源ID';
+
+Alter table `resource_item` modify COLUMN `hummer_id` varchar(1024) DEFAULT NULL COMMENT '资源ID';
+
+Alter table `cloud_resource_item` modify COLUMN `hummer_id` varchar(1024) DEFAULT NULL COMMENT '资源ID';
+
+ALTER TABLE `cloud_resource_item` modify COLUMN `hummer_name` varchar(1024) DEFAULT NULL COMMENT '资源别名';
+
+ALTER TABLE `resource_item` modify COLUMN `hummer_name` varchar(1024) DEFAULT NULL COMMENT '资源别名';
+
