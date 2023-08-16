1aa9a5a1611f703ce003b7f03edd5b0bf6d05aa5
harris1943 <ma402847591@gmail.com>
2023-08-16 19:00:09 +0800

    refactor(ui): 优化规则风险安全策略编号

--- hummer-ui/src/business/components/report/home/List.vue
+++ hummer-ui/src/business/components/report/home/List.vue
@@ -350,7 +350,7 @@
               <el-input v-model="ruleForm.description" autocomplete="off" :placeholder="$t('rule.rule_description')"/>
             </el-form-item>
             <el-form-item :label="$t('account.cloud_platform')">
-              <el-select style="width: 100%;" v-model="ruleForm.pluginId" :placeholder="$t('account.please_choose_plugin')">
+              <el-select style="width: 100%;" filterable :clearable="true" v-model="ruleForm.pluginId" :placeholder="$t('account.please_choose_plugin')">
                 <el-option
                   v-for="item in plugins"
                   :key="item.id"
@@ -362,7 +362,7 @@
               </el-select>
             </el-form-item>
             <el-form-item :label="$t('rule.rule_tag')">
-              <el-select style="width: 100%;" multiple v-model="ruleForm.tags" :placeholder="$t('rule.please_choose_tag')">
+              <el-select style="width: 100%;" multiple filterable :clearable="true" v-model="ruleForm.tags" :placeholder="$t('rule.please_choose_tag')">
                 <el-option
                   v-for="item in tags"
                   :key="item.tagKey"
@@ -372,7 +372,7 @@
               </el-select>
             </el-form-item>
             <el-form-item :label="$t('rule.severity')">
-              <el-select style="width: 100%;" v-model="ruleForm.severity" :placeholder="$t('rule.please_choose_severity')">
+              <el-select style="width: 100%;" filterable :clearable="true" v-model="ruleForm.severity" :placeholder="$t('rule.please_choose_severity')">
                 <el-option
                   v-for="item in severityOptions"
                   :key="item.value"
@@ -382,7 +382,7 @@
               </el-select>
             </el-form-item>
             <el-form-item :label="$t('rule.rule_set')">
-              <el-select style="width: 100%;" multiple filterable v-model="ruleForm.ruleSets">
+              <el-select style="width: 100%;" multiple filterable :clearable="true" v-model="ruleForm.ruleSets">
                 <el-option
                   v-for="item in ruleSetOptions"
                   :key="item.id"
@@ -392,11 +392,11 @@
               </el-select>
             </el-form-item>
             <el-form-item :label="$t('rule.inspection_report')">
-              <el-select style="width: 100%;" multiple filterable collapse-tags v-model="ruleForm.inspectionSeports">
+              <el-select style="width: 100%;" multiple filterable :clearable="true" collapse-tags v-model="ruleForm.inspectionSeports">
                 <el-option
                   v-for="item in inspectionSeportOptions"
                   :key="item.id"
-                  :label="item.id + '. ' + item.project?item.project.substring(0, 50):'' + '...'"
+                  :label="item.id + '. ' + item.project.substring(0, 50) + '...'"
                   :value="item.id">
                 </el-option>
               </el-select>
