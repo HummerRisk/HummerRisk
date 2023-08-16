5e3fe89610d09a207d1d2fa97d79e63783674ed3
harris1943 <ma402847591@gmail.com>
2023-08-16 11:45:29 +0800

    refactor(event): 优化event url路径

--- hummer-ui/src/api/cloud/event/event.js
+++ hummer-ui/src/api/cloud/event/event.js
@@ -1,28 +1,28 @@
 
-export const cloudEventSeverityChartUrl = "/cloud/cloud/event/severityChart";
+export const cloudEventSeverityChartUrl = "/cloud/event/severityChart";
 
-export const cloudEventIpAccessChartUrl = "/cloud/cloud/event/ipAccessChart/";
+export const cloudEventIpAccessChartUrl = "/cloud/event/ipAccessChart/";
 
-export const cloudEventCloudChartUrl = "/cloud/cloud/event/cloudChart";
+export const cloudEventCloudChartUrl = "/cloud/event/cloudChart";
 
-export const cloudEventRegionChartUrl = "/cloud/cloud/event/regionChart";
+export const cloudEventRegionChartUrl = "/cloud/event/regionChart";
 
-export const cloudEventDeleteUrl = "/cloud/cloud/event/delete/";
+export const cloudEventDeleteUrl = "/cloud/event/delete/";
 
-export const cloudEventListUrl = "/cloud/cloud/event/list/";
+export const cloudEventListUrl = "/cloud/event/list/";
 
-export const cloudEventGroupListUrl = "/cloud/cloud/event/group/list/";
+export const cloudEventGroupListUrl = "/cloud/event/group/list/";
 
-export const cloudEventInsightListUrl = "/cloud/cloud/event/insight/list/";
+export const cloudEventInsightListUrl = "/cloud/event/insight/list/";
 
-export const cloudEventSyncLogRegionListUrl = "/cloud/cloud/event/sync/log/region/list/";
+export const cloudEventSyncLogRegionListUrl = "/cloud/event/sync/log/region/list/";
 
-export const cloudEventSyncUrl = "/cloud/cloud/event/sync";
+export const cloudEventSyncUrl = "/cloud/event/sync";
 
-export const cloudEventSyncLogDeleteUrl = "/cloud/cloud/event/sync/log/delete/";
+export const cloudEventSyncLogDeleteUrl = "/cloud/event/sync/log/delete/";
 
-export const cloudEventTopInfoUrl = "/cloud/cloud/event/topInfo";
+export const cloudEventTopInfoUrl = "/cloud/event/topInfo";
 
-export const cloudEventSyncLogListUrl = "/cloud/cloud/event/sync/log/list/";
+export const cloudEventSyncLogListUrl = "/cloud/event/sync/log/list/";
 
-export const cloudEventDeleteLogsUrl = "/cloud/cloud/event/deleteLogs";
+export const cloudEventDeleteLogsUrl = "/cloud/event/deleteLogs";
