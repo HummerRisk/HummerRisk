0a0fe8a4692e2baa50ac7bf0e61bac09e7ef7bc2
harris1943 <ma402847591@gmail.com>
2023-08-16 11:55:07 +0800

    refactor(sync): 优化云事件同步sync url路径

--- hummer-ui/src/api/cloud/sync/sync.js
+++ hummer-ui/src/api/cloud/sync/sync.js
@@ -1,20 +1,20 @@
 
-export const cloudSyncLogItemListUrl = "/cloud/cloud/sync/log/item/list/";
+export const cloudSyncLogItemListUrl = "/cloud/sync/log/item/list/";
 
-export const cloudSyncUrl = "/cloud/cloud/sync/sync/";
+export const cloudSyncUrl = "/cloud/sync/sync/";
 
-export const cloudSyncLogListUrl = "/cloud/cloud/sync/log/list/";
+export const cloudSyncLogListUrl = "/cloud/sync/log/list/";
 
-export const cloudSyncDeleteUrl = "/cloud/cloud/sync/delete/";
+export const cloudSyncDeleteUrl = "/cloud/sync/delete/";
 
-export const cloudTopologyUrl = "/cloud/cloud/sync/cloudTopology";
+export const cloudTopologyUrl = "/cloud/sync/cloudTopology";
 
-export const cloudTopologyByAccountIdUrl = "/cloud/cloud/sync/cloudTopologyByAccountId/";
+export const cloudTopologyByAccountIdUrl = "/cloud/sync/cloudTopologyByAccountId/";
 
-export const syncResourceTypeListUrl = "/cloud/cloud/sync/resourceType/list/";
+export const syncResourceTypeListUrl = "/cloud/sync/resourceType/list/";
 
-export const syncDeleteLogsUrl = "/cloud/cloud/sync/deleteLogs";
+export const syncDeleteLogsUrl = "/cloud/sync/deleteLogs";
 
-export const cloudTopologyresourceRelaUrl = "/cloud/cloud/sync/cloudTopologyRela/";
+export const cloudTopologyresourceRelaUrl = "/cloud/sync/cloudTopologyRela/";
 
 
