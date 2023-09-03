0a5e6dac43167ab51962ae8016f45d859eba4fd4
harris1943 <ma402847591@gmail.com>
2023-09-03 22:49:28 +0800

    refactor(swagger): 去除common swagger

--- hummer-ui/src/api/project/project.js
+++
@@ -1,6 +0,0 @@
-
-export const projectScanUrl = "/cloud/project/scan";
-
-export const processListUrl = "/cloud/project/process/list";
-
-export const processCreateUrl = "/cloud/project/process/create";
