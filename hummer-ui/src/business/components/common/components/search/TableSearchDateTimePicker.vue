e31dbcc2a6d113af272e26b1b8f00984330a4dd1
harris1943 <ma402847591@gmail.com>
2023-09-06 18:32:42 +0800

    refactor(date): 优化日历插件，带快捷选项(最近一周)

--- hummer-ui/src/business/components/common/components/search/TableSearchDateTimePicker.vue
+++ hummer-ui/src/business/components/common/components/search/TableSearchDateTimePicker.vue
@@ -6,7 +6,8 @@
                       :type="type" :key="type" value-format="timestamp"
                       :range-separator="$t('commons.date.range_separator')"
                       :start-placeholder="$t('commons.date.start_date_time')"
-                      :end-placeholder="$t('commons.date.end_date_time')">
+                      :end-placeholder="$t('commons.date.end_date_time')"
+                      :picker-options="pickerOptions">
       </el-date-picker>
     </template>
   </table-search-component>
@@ -20,6 +21,37 @@ import {OPERATORS} from "./search-components"
   export default {
     name: "TableSearchDateTimePicker",
     components: {TableSearchComponent},
+    data() {
+      return {
+        pickerOptions: {
+          shortcuts: [{
+            text: '最近一周',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近一个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近三个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+              picker.$emit('pick', [start, end]);
+            }
+          }]
+        }
+      }
+    },
     props: ['component'],
     methods: {
       change(value) {
